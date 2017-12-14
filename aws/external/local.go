168287968503105322a4f29b691d16d7e5052378
Jason Del Ponte <delpontej@gmail.com>
2017-12-14 12:31:17 -0800

    update based on v1 feedback

--- aws/external/local.go
+++ aws/external/local.go
@@ -4,28 +4,30 @@ import (
 	"fmt"
 	"net"
 	"net/url"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
 )
 
 var lookupHostFn = net.LookupHost
 
-func isLoopbackHost(host string) bool {
+func isLoopbackHost(host string) (bool, error) {
 	ip := net.ParseIP(host)
 	if ip != nil {
-		return ip.IsLoopback()
+		return ip.IsLoopback(), nil
 	}
 
 	// Host is not an ip, perform lookup
 	addrs, err := lookupHostFn(host)
 	if err != nil {
-		return false
+		return false, err
 	}
 	for _, addr := range addrs {
 		if !net.ParseIP(addr).IsLoopback() {
-			return false
+			return false, nil
 		}
 	}
 
-	return true
+	return true, nil
 }
 
 func validateLocalURL(v string) error {
@@ -34,7 +36,12 @@ func validateLocalURL(v string) error {
 		return err
 	}
 
-	if host := u.Hostname(); len(host) == 0 || !isLoopbackHost(host) {
+	host := aws.URLHostname(u)
+	if len(host) == 0 {
+		return fmt.Errorf("unable to parse host from local HTTP cred provider URL")
+	} else if isLoopback, err := isLoopbackHost(host); err != nil {
+		return fmt.Errorf("failed to resolve host %q, %v", host, err)
+	} else if !isLoopback {
 		return fmt.Errorf("invalid endpoint host, %q, only host resolving to loopback addresses are allowed", host)
 	}
 
