1f1b8ce65169a5adb123ae9afc75276f69a3d1ab
Jason Del Ponte <delpontej@gmail.com>
2017-12-21 16:00:15 -0800

    gofmt

--- internal/awstesting/cmd/op_crawler/main.go
+++ internal/awstesting/cmd/op_crawler/main.go
@@ -20,8 +20,8 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/aws/defaults"
-	"github.com/aws/aws-sdk-go-v2/aws/external"
 	"github.com/aws/aws-sdk-go-v2/aws/endpoints"
+	"github.com/aws/aws-sdk-go-v2/aws/external"
 )
 
 func main() {
