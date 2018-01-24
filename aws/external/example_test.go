90521c862d425d64a8919858ff524c783b5f913e
Jason Del Ponte <delpontej@gmail.com>
2018-01-24 11:28:56 -0800

    add MFA token example

--- aws/external/example_test.go
+++ aws/external/example_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/aws/stscreds"
 )
 
 func ExampleWithSharedConfigProfile() {
@@ -57,4 +58,23 @@ func ExampleWithCredentialsValue() {
 
 	// Output:
 	// Credentials Source: example hard coded credentials
+}
+
+func ExampleWithMFATokenFunc() {
+	cfg, err := external.LoadDefaultAWSConfig(
+		// Set the provider function for the MFA token.
+		external.WithMFATokenFunc(stscreds.StdinTokenProvider),
+
+		// Optionally, specify the shared configuration profile to load.
+		external.WithSharedConfigProfile("exampleProfile"),
+	)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to load config, %v", err)
+		os.Exit(1)
+	}
+
+	// If assume role credentials with MFA enabled are specified in the shared
+	// 	configuration the MFA token provider function will be called to retrieve
+	// the MFA token for the assume role API call.
+	fmt.Println(cfg.Credentials.Retrieve())
 }
