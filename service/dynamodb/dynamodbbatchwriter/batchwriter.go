187cf7dc1d0a711f6ba6227dc6dc9d744def8a24
IanTayler <iangtayler@gmail.com>
2018-04-02 00:25:35 -0300

    dynamodb: Add BatchWriter.

---
+++ service/dynamodb/dynamodbbatchwriter/batchwriter.go
@@ -0,0 +1,103 @@
+package dynamodbbatchwriter
+
+import (
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/dynamodbiface"
+)
+
+const defaultFlushAmount = 25
+const defaultRequestBufferCap = 50
+
+// BatchWriter wraps a dynamodb client to expose a simple PutItem/DeleteItem
+// API that buffers requests and takes advantage of BatchWriteItem behind
+// the scenes.
+type BatchWriter struct {
+	_ struct{}
+
+	tableName     string
+	client        dynamodbiface.DynamoDBAPI
+	flushAmount   int
+	requestBuffer []dynamodb.WriteRequest
+}
+
+// New creates a new BatchWriter that will write to table `tableName`
+// using `client`.
+func New(tableName string, client dynamodbiface.DynamoDBAPI) *BatchWriter {
+	requestBuffer := make(
+		[]dynamodb.WriteRequest, 0, defaultRequestBufferCap,
+	)
+	return &BatchWriter{
+		tableName:     tableName,
+		client:        client,
+		flushAmount:   defaultFlushAmount,
+		requestBuffer: requestBuffer,
+	}
+}
+
+// SetFlushAmount changes the size at which the requestBuffer gets `Flush`ed
+// automatically.
+func (b *BatchWriter) SetFlushAmount(flushAmount int) {
+	b.flushAmount = flushAmount
+}
+
+func (b *BatchWriter) flushIfNeeded() {
+	if len(b.requestBuffer) >= b.flushAmount {
+		b.Flush()
+	}
+}
+
+func (b *BatchWriter) addWriteRequest(wr dynamodb.WriteRequest) {
+	b.requestBuffer = append(b.requestBuffer, wr)
+	b.flushIfNeeded()
+}
+
+// PutItem adds a PutRequest operation to the requestBuffer.
+func (b *BatchWriter) PutItem(item map[string]dynamodb.AttributeValue) {
+	putRequest := &dynamodb.PutRequest{Item: item}
+	writeRequest := dynamodb.WriteRequest{PutRequest: putRequest}
+	b.addWriteRequest(writeRequest)
+}
+
+// DeleteItem adds a DeleteRequest operation to the requestBuffer.
+//
+// The key argument should have the form of the Key argument the normal
+// DeleteItem API call takes.
+func (b *BatchWriter) DeleteItem(key map[string]dynamodb.AttributeValue) {
+	deleteRequest := &dynamodb.DeleteRequest{Key: key}
+	writeRequest := dynamodb.WriteRequest{DeleteRequest: deleteRequest}
+	b.addWriteRequest(writeRequest)
+}
+
+// Flush makes a BatchWriteItem with some or all requests that have been added
+// so far to the buffer by means of PutItem and DeleteIem.
+//
+// Any unprocessed items sent in the response will be added to the end of the
+// buffer, to be sent at a later date.
+func (b *BatchWriter) Flush() error {
+	if len(b.requestBuffer) > 0 {
+		flushBound := b.flushAmount
+		if flushBound > len(b.requestBuffer) {
+			flushBound = len(b.requestBuffer)
+		}
+		itemsToSend := b.requestBuffer[:flushBound]
+		b.requestBuffer = b.requestBuffer[flushBound:]
+		requestItems := make(map[string][]dynamodb.WriteRequest)
+		requestItems[b.tableName] = itemsToSend
+		batchInput := dynamodb.BatchWriteItemInput{RequestItems: requestItems}
+		batchRequest := b.client.BatchWriteItemRequest(&batchInput)
+		output, err := batchRequest.Send()
+		if err != nil {
+			return err
+		}
+		unpItems, ok := output.UnprocessedItems[b.tableName]
+		if ok {
+			b.requestBuffer = append(b.requestBuffer, unpItems...)
+		}
+	}
+	return nil
+}
+
+// Empty returns whether or not the request buffer is empty.
+func (b *BatchWriter) Empty() bool {
+	return len(b.requestBuffer) <= 0
+}
