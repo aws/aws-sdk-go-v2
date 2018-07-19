e9d3654c7c254ccdc09a2a46ef82d8d1353f6b0e
Jason Del Ponte <delpontej@gmail.com>
2018-07-19 14:04:58 -0700

    service/s3/s3manager: Add doc for sequential download

--- service/s3/s3manager/download.go
+++ service/s3/s3manager/download.go
@@ -38,6 +38,8 @@ type Downloader struct {
 	// The number of goroutines to spin up in parallel when sending parts.
 	// If this is set to zero, the DefaultDownloadConcurrency value will be used.
 	//
+	// Concurrency of 1 will download the parts sequentially.
+	//
 	// Concurrency is ignored if the Range input parameter is provided.
 	Concurrency int
 
@@ -148,6 +150,9 @@ func NewDownloaderWithClient(svc s3iface.S3API, options ...func(*Downloader)) *D
 // The w io.WriterAt can be satisfied by an os.File to do multipart concurrent
 // downloads, or in memory []byte wrapper using aws.WriteAtBuffer.
 //
+// Specifying a Downloader.Concurrency of 1 will cause the Downloader to
+// download the parts from S3 sequentially.
+//
 // If the GetObjectInput's Range value is provided that will cause the downloader
 // to perform a single GetObjectInput request for that object's range. This will
 // caused the part size, and concurrency configurations to be ignored.
@@ -172,6 +177,9 @@ func (d Downloader) Download(w io.WriterAt, input *s3.GetObjectInput, options ..
 //
 // The w io.WriterAt can be satisfied by an os.File to do multipart concurrent
 // downloads, or in memory []byte wrapper using aws.WriteAtBuffer.
+//
+// Specifying a Downloader.Concurrency of 1 will cause the Downloader to
+// download the parts from S3 sequentially.
 //
 // It is safe to call this method concurrently across goroutines.
 //
