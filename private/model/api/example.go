0734f397d9e9b7ea27e7076d0621a960d202ed1b
Shantanu Kotambkar <52007797+skotambkar@users.noreply.github.com>
2019-12-12 16:22:49 -0800

    private/model: removes customization for merging services (#454)
    The SDK has customizations for merging services like dynamodbstreams with dynamodb; and waf-regional with waf service. This was intended to avoid duplicate shapes. As the SDK moves towards a layout with types, and enums package as seperate package, we need to remove these customizations as they provide little to no value and add complications. This PR removes those unnecessary customizations.

--- private/model/api/example.go
+++ private/model/api/example.go
@@ -227,17 +227,8 @@ func (a *API) AttachExamples(filename string) {
 	p.setup()
 }
 
-var examplesBuilderCustomizations = map[string]examplesBuilder{
-	"wafregional": wafregionalExamplesBuilder{},
-}
-
 func (p *ExamplesDefinition) setup() {
-	var builder examplesBuilder
-	ok := false
-	if builder, ok = examplesBuilderCustomizations[p.API.PackageName()]; !ok {
-		builder = defaultExamplesBuilder{}
-	}
-
+	builder := defaultExamplesBuilder{}
 	keys := p.Examples.Names()
 	for _, n := range keys {
 		examples := p.Examples[n]
@@ -288,12 +279,7 @@ type exHeader struct {
 // examples.json file.
 func (a *API) ExamplesGoCode() string {
 	var buf bytes.Buffer
-	var builder examplesBuilder
-	ok := false
-	if builder, ok = examplesBuilderCustomizations[a.PackageName()]; !ok {
-		builder = defaultExamplesBuilder{}
-	}
-
+	builder := defaultExamplesBuilder{}
 	if err := exampleHeader.ExecuteTemplate(&buf, "exampleHeader", &exHeader{builder, a}); err != nil {
 		panic(err)
 	}
