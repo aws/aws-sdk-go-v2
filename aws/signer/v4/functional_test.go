6c65993ee6f060748ec2fda4cfe17eb44916f525
Jason Del Ponte <delpontej@gmail.com>
2020-03-09 14:52:33 -0700

    cleanup uneeded change

--- aws/signer/v4/functional_test.go
+++ aws/signer/v4/functional_test.go
@@ -12,7 +12,7 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/aws/endpoints"
-	v4 "github.com/aws/aws-sdk-go-v2/aws/signer/v4"
+	"github.com/aws/aws-sdk-go-v2/aws/signer/v4"
 	"github.com/aws/aws-sdk-go-v2/internal/awstesting/unit"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
