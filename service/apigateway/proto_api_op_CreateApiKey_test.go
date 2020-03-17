a3eb6d0609f8b5b6bb08dd71646e3cca6f53789c
skotambkar <kotambka@amazon.com>
2020-03-17 10:41:46 -0700

    fix failing aseertion

--- service/apigateway/proto_api_op_CreateApiKey_test.go
+++ service/apigateway/proto_api_op_CreateApiKey_test.go
@@ -1,8 +1,11 @@
 package apigateway_test
 
 import (
-	"bytes"
 	"context"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"reflect"
 	"testing"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
@@ -45,7 +48,40 @@ func TestProtoCreateApiKeyRequestMarshaler_Diff(t *testing.T) {
 	if diff := cmp.Diff(request.HTTPRequest.URL, prototypeRequest.HTTPRequest.URL); diff != "" {
 		t.Errorf("Found diff: %v", diff)
 	}
-	if diff := cmp.Diff(request.Body, prototypeRequest.Body, cmp.AllowUnexported(bytes.Reader{})); diff != "" {
-		t.Errorf("Found diff: %v", diff)
+
+	reqBody, err := request.HTTPRequest.GetBody()
+	if err != nil {
+		t.Fatal("failed to read body from request", err)
+	}
+
+	protoBody, err := prototypeRequest.HTTPRequest.GetBody()
+	if err != nil {
+		t.Fatal("failed to read body from prototyped request", err)
+	}
+	assertJSON(t, reqBody, protoBody)
+}
+
+func assertJSON(t testing.TB, a, b io.ReadCloser) {
+	t.Helper()
+	buf, err := ioutil.ReadAll(a)
+	if err != nil {
+		t.Fatal("Error reading a", err)
+	}
+	protoBuff, err := ioutil.ReadAll(b)
+	if err != nil {
+		t.Fatal("Error reading b", err)
+	}
+	var av, bv interface{}
+
+	if err := json.Unmarshal(buf, &av); err != nil {
+		t.Fatalf("assertJSON: unable to unmarshal a, %v", err)
+	}
+
+	if err := json.Unmarshal(protoBuff, &bv); err != nil {
+		t.Fatalf("assertJSON: unable to unmarshal b, %v", err)
+	}
+
+	if !reflect.DeepEqual(av, bv) {
+		t.Fatalf("JSON are not equal\nexpect:\n%v\nactual:\n%v\n", a, b)
 	}
 }
