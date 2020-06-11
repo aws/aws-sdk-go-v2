cb268a88dabf953ec5f2e44d61e4f85e0ea94dd5
Sean McGrail <mcgrails@amazon.com>
2020-06-10 19:06:52 -0700

    Refactor GoDependency and constants

---
+++ codegen/smithy-aws-go-codegen/src/main/java/software/amazon/smithy/aws/go/codegen/AwsGoDependency.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package software.amazon.smithy.aws.go.codegen;
+
+import software.amazon.smithy.go.codegen.GoDependency;
+
+/**
+ * An enum of all the built-in dependencies used by this package.
+ */
+public enum AwsGoDependency {
+    AWS_REST_JSON_PROTOCOL(aws("aws/protocol/restjson"));
+
+    public static final String AWS_SOURCE_PATH = "github.com/aws/aws-sdk-go-v2";
+
+    private final GoDependency dependency;
+
+    AwsGoDependency(GoDependency dependency) {
+        this.dependency = dependency;
+    }
+
+    public GoDependency getDependency() {
+        return dependency;
+    }
+    private static GoDependency aws(String relativePath) {
+        return aws(relativePath, null);
+    }
+
+    private static GoDependency aws(String relativePath, String alias) {
+        String importPath = AWS_SOURCE_PATH;
+        if (relativePath != null) {
+            importPath = importPath + "/" + relativePath;
+        }
+        return GoDependency.moduleDependency(AWS_SOURCE_PATH, importPath, Versions.AWS_SDK, alias);
+    }
+
+    private static final class Versions {
+        private static final String AWS_SDK = "v0.0.0-20200608172716-6b3036355dc9";
+    }
+}
