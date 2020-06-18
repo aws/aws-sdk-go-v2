fc1afee786d638cbbfadd978566cb2b4975202e9
Sean McGrail <mcgrails@amazon.com>
2020-06-18 13:16:16 -0700

    Metadata and Endpoint Middleware, Signing Changes

---
+++ aws/middleware/endpoints.go
@@ -0,0 +1,66 @@
+package middleware
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/awslabs/smithy-go/middleware"
+	smithyhttp "github.com/awslabs/smithy-go/transport/http"
+)
+
+// ResolveServiceEndpoint is a middleware that will resolve the service endpoint.
+type ResolveServiceEndpoint struct {
+	Resolver aws.EndpointResolver
+}
+
+// ID is a middleware identifier
+func (r ResolveServiceEndpoint) ID() string {
+	return "ResolveServiceEndpoint"
+}
+
+// HandleSerialize resolves the service endpoint and returns
+func (r ResolveServiceEndpoint) HandleSerialize(
+	ctx context.Context, in middleware.SerializeInput, next middleware.SerializeHandler,
+) (out middleware.SerializeOutput, metadata middleware.Metadata, err error) {
+	req, ok := in.Request.(*smithyhttp.Request)
+	if !ok {
+		return out, metadata, fmt.Errorf("unknown transport type %T", in.Request)
+	}
+
+	var endpoint aws.Endpoint
+	endpoint, err = r.Resolver.ResolveEndpoint(GetServiceName(ctx), GetEndpointID(ctx))
+	if err != nil {
+		return out, metadata, fmt.Errorf("failed to resolve service endpoint ")
+	}
+
+	operationMetadata := GetOperationMetadata(ctx)
+	req.URL, err = url.Parse(endpoint.URL + operationMetadata.HTTPPath)
+	if err != nil {
+		return out, metadata, fmt.Errorf("failed to parse endpoint URL: %w", err)
+	}
+
+	ctx = SetSigningRegion(ctx, endpoint.SigningRegion)
+	return next.HandleSerialize(ctx, in)
+}
+
+// ResolveServiceEndpointOptions is an interface for retrieving options to configure a ResolveServiceEndpoint middleware.
+type ResolveServiceEndpointOptions interface {
+	GetResolver() aws.EndpointResolver
+}
+
+// AddResolveServiceEndpointMiddleware creates a ResolveServiceEndpoint middleware with the provided options
+// and registers it on the provided stack.
+func AddResolveServiceEndpointMiddleware(stack *middleware.Stack, options ResolveServiceEndpointOptions) error {
+	m := ResolveServiceEndpoint{
+		Resolver: options.GetResolver(),
+	}
+	return stack.Serialize.Add(m, middleware.Before)
+}
+
+// RemoveResolveServiceEndpointMiddleware removes the ResolveServiceEndpoint middleware from the provided stack.
+func RemoveResolveServiceEndpointMiddleware(stack *middleware.Stack) error {
+	id := ResolveServiceEndpoint{}.ID()
+	return stack.Serialize.Remove(id)
+}
--- codegen/smithy-aws-go-codegen/src/main/java/software/amazon/smithy/aws/go/codegen/AssembleMiddlewareStack.java
+++ codegen/smithy-aws-go-codegen/src/main/java/software/amazon/smithy/aws/go/codegen/AssembleMiddlewareStack.java
@@ -1,6 +1,9 @@
 package software.amazon.smithy.aws.go.codegen;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import software.amazon.smithy.aws.traits.ServiceTrait;
 import software.amazon.smithy.aws.traits.auth.SigV4Trait;
 import software.amazon.smithy.aws.traits.auth.UnsignedPayloadTrait;
 import software.amazon.smithy.codegen.core.Symbol;
@@ -11,9 +14,11 @@ import software.amazon.smithy.go.codegen.integration.GoIntegration;
 import software.amazon.smithy.go.codegen.integration.ProtocolGenerator;
 import software.amazon.smithy.go.codegen.integration.RuntimeClientPlugin;
 import software.amazon.smithy.model.traits.AuthTrait;
+import software.amazon.smithy.model.traits.HttpTrait;
+import software.amazon.smithy.model.traits.TitleTrait;
 import software.amazon.smithy.utils.ListUtils;
 
-public class AssembleMiddlewareStack implements GoIntegration{
+public class AssembleMiddlewareStack implements GoIntegration {
 
     private static final String INITIALIZE_MIDDLEWARE = "Initialize";
     private static final String SERIALIZE_MIDDLEWARE = "Serialize";
@@ -23,18 +28,19 @@ public class AssembleMiddlewareStack implements GoIntegration{
 
     /**
      * Generates code to add middleware at the end in operation stack step.
-     * @param writer writer used to write Go code.
-     * @param stackstep stack step where the middleware is to be added.
+     *
+     * @param writer           writer used to write Go code.
+     * @param stackstep        stack step where the middleware is to be added.
      * @param middlewareSymbol middleware symbol corresponding to middleware to be added.
-     * @param content Gowriter content used for generation.
-     * @param stackOperand stack operand to which middleware is to be added.
+     * @param content          Gowriter content used for generation.
+     * @param stackOperand     stack operand to which middleware is to be added.
      */
     private void writeAddMiddlewareAfter(
-        GoWriter writer,
-        String stackstep,
-        Symbol middlewareSymbol,
-        String content,
-        String stackOperand
+            GoWriter writer,
+            String stackstep,
+            Symbol middlewareSymbol,
+            String content,
+            String stackOperand
     ) {
         String st = String.format("%s.%s.Add(%s, middleware.After)", stackOperand, stackstep, content);
         writer.write(st, middlewareSymbol);
@@ -43,11 +49,12 @@ public class AssembleMiddlewareStack implements GoIntegration{
 
     /**
      * Generates code to add middleware at the end in operation stack step.
-     * @param writer writer used to write Go code.
-     * @param stackstep stack step where the middleware is to be added.
+     *
+     * @param writer         writer used to write Go code.
+     * @param stackstep      stack step where the middleware is to be added.
      * @param middlewareName middleware name corresponding to middleware to be added.
-     * @param content Gowriter content used for generation.
-     * @param stackOperand stack operand to which middleware is to be added.
+     * @param content        Gowriter content used for generation.
+     * @param stackOperand   stack operand to which middleware is to be added.
      */
     private void writeAddMiddlewareAfter(
             GoWriter writer,
@@ -63,11 +70,12 @@ public class AssembleMiddlewareStack implements GoIntegration{
 
     /**
      * Generates code to add middleware at the beginning in operation stack step.
-     * @param writer writer used to write Go code.
-     * @param stackstep stack step where the middleware is to be added.
+     *
+     * @param writer           writer used to write Go code.
+     * @param stackstep        stack step where the middleware is to be added.
      * @param middlewareSymbol middleware symbol corresponding to middleware to be added.
-     * @param content Gowriter content used for generation.
-     * @param stackOperand stack operand to which middleware is to be added.
+     * @param content          Gowriter content used for generation.
+     * @param stackOperand     stack operand to which middleware is to be added.
      */
     private void writeAddMiddlewareBefore(
             GoWriter writer,
@@ -78,28 +86,72 @@ public class AssembleMiddlewareStack implements GoIntegration{
     ) {
         String st = String.format("%s.%s.Add(%s, middleware.Before)", stackOperand, stackstep, content);
         writer.write(st, middlewareSymbol);
-        writer.addUseImports(SmithyGoDependency.SMITHY_MIDDLEWARE);
     }
 
     @Override
     public List<RuntimeClientPlugin> getClientPlugins() {
         return ListUtils.of(
+                // Add ServiceMetadataProvider to operation stack
+                RuntimeClientPlugin.builder()
+                        .buildMiddlewareStack((writer, service, operation, protocolGenerator, stackOperand) -> {
+                            Symbol ServiceMetadataProvider = SymbolUtils.createValueSymbolBuilder(
+                                    "RegisterServiceMetadata", AwsGoDependency.AWS_MIDDLEWARE).build();
+
+                            Optional<ServiceTrait> serviceTrait = service.getTrait(ServiceTrait.class);
+                            Optional<SigV4Trait> sigV4Trait = service.getTrait(SigV4Trait.class);
+
+                            StringBuilder builder = new StringBuilder();
+                            builder.append("$T{\n");
+                            builder.append("Region: options.Region,\n");
+                            if (serviceTrait.isPresent()) {
+                                ServiceTrait trait = serviceTrait.get();
+                                String sdkAsSymbol = trait.getSdkId().toLowerCase().replaceAll("\\s+", "");
+                                builder.append(String.format("ServiceName: \"%s\",\n", trait.getSdkId()));
+                                builder.append(String.format("ServiceID: \"%s\",\n", sdkAsSymbol));
+                                // TODO: EndpointID can be different but is not modeled in Smithy.
+                                builder.append(String.format("EndpointID: \"%s\",\n", sdkAsSymbol));
+                            }
+                            if (sigV4Trait.isPresent()) {
+                                SigV4Trait trait = sigV4Trait.get();
+                                builder.append(String.format("SigningName: \"%s\",\n", trait.getName()));
+                            }
+                            builder.append("Operation: awsmiddleware.OperationMetadata{\n"
+                                    + String.format("Name: \"%s\",\n", operation.getId().getName())
+                                    + String.format("HTTPPath: \"%s\",\n", operation.getTrait(HttpTrait.class)
+                                    .map(httpTrait -> httpTrait.getUri().toString())
+                                    .orElse("/"))
+                                    + "},\n");
+                            builder.append("}");
+
+                            writeAddMiddlewareBefore(writer, INITIALIZE_MIDDLEWARE, ServiceMetadataProvider,
+                                    builder.toString(), stackOperand);
+                        })
+                        .build(),
+
                 // Add RequestInvocationIDMiddleware to operation stack
                 RuntimeClientPlugin.builder()
-                    .buildMiddlewareStack((writer, service, operation, protocolGenerator, stackOperand) -> {
-                        // RequestInvocationIDMiddleware
-                        Symbol RequestInvocationIDMiddleware = SymbolUtils.createValueSymbolBuilder(
-                                "RequestInvocationIDMiddleware", AwsGoDependency.AWS_MIDDLEWARE
-                        ).build();
+                        .buildMiddlewareStack((writer, service, operation, protocolGenerator, stackOperand) -> {
+                            // RequestInvocationIDMiddleware
+                            Symbol RequestInvocationIDMiddleware = SymbolUtils.createValueSymbolBuilder(
+                                    "RequestInvocationIDMiddleware", AwsGoDependency.AWS_MIDDLEWARE
+                            ).build();
 
-                        writeAddMiddlewareAfter(writer, BUILD_MIDDLEWARE,
-                                RequestInvocationIDMiddleware, "$T{}", stackOperand);
-                    }).build(),
+                            writeAddMiddlewareAfter(writer, BUILD_MIDDLEWARE,
+                                    RequestInvocationIDMiddleware, "$T{}", stackOperand);
+                        }).build(),
+
+                // Add endpoint serialize middleware to operation stack
+                RuntimeClientPlugin.builder()
+                        .buildMiddlewareStack((writer, service, operation, protocolGenerator, stackOperand) -> {
+                            Symbol endpointMiddleware = SymbolUtils.createValueSymbolBuilder(
+                                    "AddResolveServiceEndpointMiddleware", AwsGoDependency.AWS_MIDDLEWARE).build();
+                            writer.write("$T(stack, options)", endpointMiddleware);
+                        }).build(),
 
                 // Add serializer middleware to operation stack
                 RuntimeClientPlugin.builder()
                         .buildMiddlewareStack((writer, service, operation, protocolGenerator, stackOperand) -> {
-                            if (protocolGenerator == null){
+                            if (protocolGenerator == null) {
                                 return;
                             }
                             String serializerMiddlewareName = ProtocolGenerator.getSerializeMiddlewareName(operation.getId(),
@@ -141,7 +193,8 @@ public class AssembleMiddlewareStack implements GoIntegration{
                             ).build();
 
                             writeAddMiddlewareAfter(writer, FINALIZE_MIDDLEWARE,
-                                    newAttemptMiddleware, "$T(options.Retryer, smithyhttp.RequestCloner)", stackOperand);
+                                    newAttemptMiddleware, "$T(options.Retryer, smithyhttp.RequestCloner)",
+                                    stackOperand);
                         }).build(),
 
                 // Add retry middleware to operation stack
@@ -178,8 +231,8 @@ public class AssembleMiddlewareStack implements GoIntegration{
                 RuntimeClientPlugin.builder()
                         .operationPredicate((model, service, operation) -> {
                             if (service.hasTrait(SigV4Trait.class) && (!operation.hasTrait(UnsignedPayloadTrait.class))
-                                   && (operation.hasTrait(SigV4Trait.class) || !operation.hasTrait(AuthTrait.class))
-                            ){
+                                    && (operation.hasTrait(SigV4Trait.class) || !operation.hasTrait(AuthTrait.class))
+                            ) {
                                 return true;
                             }
                             return false;
