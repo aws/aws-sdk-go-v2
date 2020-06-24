d244b213478321538b74be9b6bf8f6dd7127b1fd
Jason Del Ponte <delpontej@gmail.com>
2020-06-24 10:15:24 -0700

    Add add sort order to implementations of GoIntegrations

--- codegen/smithy-aws-go-codegen/src/main/java/software/amazon/smithy/aws/go/codegen/AddProtocols.java
+++ codegen/smithy-aws-go-codegen/src/main/java/software/amazon/smithy/aws/go/codegen/AddProtocols.java
@@ -7,6 +7,17 @@ import software.amazon.smithy.utils.ListUtils;
 import java.util.List;
 
 public class AddProtocols implements GoIntegration {
+    /**
+ 	 * Gets the sort order of the customization from -128 to 127, with lowest
+	 * executed first.
+     *
+     * @return Returns the sort order, defaults to -10.
+     */
+    @Override
+    public byte getOrder() {
+        return -10;
+    }
+
     @Override
     public List<ProtocolGenerator> getProtocolGenerators() {
         return ListUtils.of(new AwsRestJson1());
