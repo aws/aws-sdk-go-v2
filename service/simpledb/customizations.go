28c340fdb993dea01b6d1fe0815e6d5a352ea3b1
JordonPhillips <phjordon@amazon.com>
2020-07-07 15:55:00 -0700

    Remove all services that aren't referenced
    This removes all services that aren't referenced by tests or features
    so that the newly generated service modules can take their place.

--- service/simpledb/customizations.go
+++
@@ -1,9 +0,0 @@
-package simpledb
-
-func init() {
-	initClient = func(c *Client) {
-		// SimpleDB uses custom error unmarshaling logic
-		c.Handlers.UnmarshalError.Clear()
-		c.Handlers.UnmarshalError.PushBack(unmarshalError)
-	}
-}
