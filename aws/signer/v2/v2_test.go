baed12950117108263ee5e0a52dd6256a52cb7f9
Sean McGrail <mcgrails@amazon.com>
2020-07-20 11:35:25 -0700

    Fix vet issues

--- aws/signer/v2/v2_test.go
+++ aws/signer/v2/v2_test.go
@@ -32,7 +32,7 @@ func (sb signerBuilder) BuildSigner() signer {
 		reader := bytes.NewReader(body)
 		req, _ = http.NewRequest(sb.Method, endpoint, reader)
 		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-		req.Header.Add("Content-Length", strconv.FormatInt(int64(len(body)), 10))
+		req.Header.Add("Content-Length", strconv.Itoa(len(body)))
 	} else {
 		req, _ = http.NewRequest(sb.Method, endpoint, nil)
 		req.URL.RawQuery = sb.Query.Encode()
