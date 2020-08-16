eb783c4c77c4105dfcf09d73b246b2d5c464cb0f
Kalaiarasan B <kbalaraman@altimetrik.com>
2020-08-16 13:15:29 +0530

    This willl fix

--- aws/external/credential_config_test.go
+++ aws/external/credential_config_test.go
@@ -25,11 +25,11 @@ func CreateIfNotExist(filepath string) error {
 
 func TestAddProfileCredentials(t *testing.T) {
 	t.Run("TestAddProfileCredentials1", func(t *testing.T) {
-		path := DefaultSharedCredentialsFilename()
-		CreateIfNotExist(path)
+		// path := DefaultSharedCredentialsFilename()
+		// CreateIfNotExist(path)
 		AccessIdTest := "AccessIdTest"
 		SecretKeyTest := "SecretKeyTest"
-		ProfileTest := "Addfirsttest"
+		ProfileTest := "Addfirsttest1"
 		c := &Credentials{Profile: &ProfileTest, AccessKeyId: &AccessIdTest, SecretAccessKey: &SecretKeyTest}
 		got, _ := AddProfileCredentials(c)
 		want := true
@@ -41,7 +41,7 @@ func TestAddProfileCredentials(t *testing.T) {
 }
 func TestDeleteProfileCredentials(t *testing.T) {
 	t.Run("TestDeleteProfileCredentials", func(t *testing.T) {
-		got, _ := DeleteProfileCredentials("Addfirsttest")
+		got, _ := DeleteProfileCredentials("Addfirsttest1")
 		want := true
 		if got != want {
 			t.Errorf("Got: %v - want: %v", got, want)
@@ -52,9 +52,9 @@ func TestDeleteProfileCredentials(t *testing.T) {
 
 func TestAddProfileConfig(t *testing.T) {
 	t.Run("TestAddProfileConfig", func(t *testing.T) {
-		path := DefaultSharedConfigFilename()
-		CreateIfNotExist(path)
-		ProfiletTest := "firstconfig"
+		// path := DefaultSharedConfigFilename()
+		// CreateIfNotExist(path)
+		ProfiletTest := "firstconfig1"
 		RegionTest := "us-east-1"
 		OutputTest := "json"
 		c := &CredentialsConfig{Profile: &ProfiletTest, Region: &RegionTest, Output: &OutputTest}
@@ -67,13 +67,13 @@ func TestAddProfileConfig(t *testing.T) {
 
 }
 
-// func TestDeleteProfileConfig(t *testing.T) {
-// 	t.Run("TestDeleteprofileconfig", func(t *testing.T) {
-// 		got, _ := DeleteProfileConfig("firstconfig")
-// 		want := true
-// 		if got != want {
-// 			t.Errorf("Got: %v - want: %v", got, want)
-// 		}
-// 	})
+func TestDeleteProfileConfig(t *testing.T) {
+	t.Run("TestDeleteprofileconfig", func(t *testing.T) {
+		got, _ := DeleteProfileConfig("firstconfig1")
+		want := true
+		if got != want {
+			t.Errorf("Got: %v - want: %v", got, want)
+		}
+	})
 
-// }
+}
