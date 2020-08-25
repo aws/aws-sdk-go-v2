e9f78c62ea0323b20453de4f58c8bcb541a7a5a4
skotambkar <kotambka@amazon.com>
2020-08-25 13:52:52 -0700

    corrects gohash

--- codegen/smithy-aws-go-codegen/src/main/java/software/amazon/smithy/aws/go/codegen/AwsGoDependency.java
+++ codegen/smithy-aws-go-codegen/src/main/java/software/amazon/smithy/aws/go/codegen/AwsGoDependency.java
@@ -51,6 +51,6 @@ public final class AwsGoDependency {
     }
 
     private static final class Versions {
-        private static final String AWS_SDK = "v0.0.0-20200825173225-662e4f915a66";
+        private static final String AWS_SDK = "v0.0.0-20200825204738-85a040a34371";
     }
 }
