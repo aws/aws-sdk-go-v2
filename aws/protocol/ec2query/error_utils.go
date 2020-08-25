662e4f915a66b8aef0eebe44a5954d94390f6ad9
skotambkar <kotambka@amazon.com>
2020-08-25 10:32:25 -0700

    feedback around fetching error code from xml doc body

--- aws/protocol/ec2query/error_utils.go
+++ aws/protocol/ec2query/error_utils.go
@@ -4,7 +4,6 @@ import (
 	"encoding/xml"
 	"fmt"
 	"io"
-	"io/ioutil"
 )
 
 // errorResponse denotes the error response structure for ec2Query error response
@@ -13,16 +12,11 @@ type errorResponse struct {
 	Message string `xml:"Errors>Error>Message"`
 }
 
-// GetEc2QueryResponseErrorCode returns the error code, error message from an ec2query error response body
+// GetResponseErrorCode returns the error code, error message from an ec2query error response body
 func GetResponseErrorCode(r io.Reader) (code string, message string, err error) {
-	rb, err := ioutil.ReadAll(r)
-	if err != nil {
-		return "", "", err
-	}
-
 	var er errorResponse
-	if err := xml.Unmarshal(rb, &er); err != nil {
-		return "", "", fmt.Errorf("error while fetching xml error response code: %w", err)
+	if err := xml.NewDecoder(r).Decode(&er); err != nil {
+		return code, message, fmt.Errorf("error while fetching xml error response code: %w", err)
 	}
 	return er.Code, er.Message, nil
 }
