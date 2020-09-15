031980348ffa25b463cb59dbfe8b04e3a5f5f444
skotambkar <kotambka@amazon.com>
2020-09-15 10:18:41 -0700

    use tabular format for tests

--- service/s3/internal/customizations/update_endpoint_test.go
+++ service/s3/internal/customizations/update_endpoint_test.go
@@ -19,80 +19,70 @@ type s3BucketTest struct {
 	err    string
 }
 
-var (
-	virtualHostStyleTests = []s3BucketTest{
-		{"abc", "https://abc.s3.mock-region.amazonaws.com/", ""},
-		{"a$b$c", "https://s3.mock-region.amazonaws.com/a%24b%24c", ""},
-		{"a.b.c", "https://s3.mock-region.amazonaws.com/a.b.c", ""},
-		{"a..bc", "https://s3.mock-region.amazonaws.com/a..bc", ""},
-	}
-
-	pathStyleTests = []s3BucketTest{
-		{"abc", "https://s3.mock-region.amazonaws.com/abc", ""},
-		{"a$b$c", "https://s3.mock-region.amazonaws.com/a%24b%24c", ""},
-		{"a.b.c", "https://s3.mock-region.amazonaws.com/a.b.c", ""},
-		{"a..bc", "https://s3.mock-region.amazonaws.com/a..bc", ""},
-	}
-)
-
-var unitcreds = aws.StaticCredentialsProvider{
-	Value: aws.Credentials{
-		AccessKeyID: "AKID", SecretAccessKey: "SECRET", SessionToken: "SESSION",
-		Source: "unit test credentials",
-	},
-}
-
-func TestPathStyleBucketBuild(t *testing.T) {
-	options := s3.Options{
-		Credentials:  unitcreds,
-		Retryer:      aws.NoOpRetryer{},
-		Region:       "mock-region",
-		UsePathStyle: true,
-	}
-
-	s := s3.New(options)
-	runTests(t, s, pathStyleTests)
-}
-
-func TestHostStyleBucketBuild(t *testing.T) {
-	options := s3.Options{
-		Credentials: unitcreds,
-		Retryer:     aws.NoOpRetryer{},
-		Region:      "mock-region",
-	}
-
-	s := s3.New(options)
-	runTests(t, s, virtualHostStyleTests)
-}
-
-func runTests(t *testing.T, svc *s3.Client, tests []s3BucketTest) {
-	for i, test := range tests {
-		fm := requestRetrieverMiddleware{}
-		_, err := svc.ListObjects(context.Background(),
-			&s3.ListObjectsInput{Bucket: &test.bucket},
-			func(options *s3.Options) {
-				options.APIOptions = append(options.APIOptions, func(stack *middleware.Stack) error {
-					stack.Serialize.Insert(&fm, "OperationSerializer", middleware.Before)
-					return nil
-				})
+func TestUpdateEndpointBuild(t *testing.T) {
+	cases := map[string]struct {
+		usePathStyle bool
+		tests        []s3BucketTest
+	}{
+		"PathStyleBucket": {
+			usePathStyle: true,
+			tests: []s3BucketTest{
+				{"abc", "https://s3.mock-region.amazonaws.com/abc", ""},
+				{"a$b$c", "https://s3.mock-region.amazonaws.com/a%24b%24c", ""},
+				{"a.b.c", "https://s3.mock-region.amazonaws.com/a.b.c", ""},
+				{"a..bc", "https://s3.mock-region.amazonaws.com/a..bc", ""},
 			},
-		)
+		},
+		"VirtualHostStyleBucket": {
+			tests: []s3BucketTest{
+				{"abc", "https://abc.s3.mock-region.amazonaws.com/", ""},
+				{"a$b$c", "https://s3.mock-region.amazonaws.com/a%24b%24c", ""},
+				{"a.b.c", "https://s3.mock-region.amazonaws.com/a.b.c", ""},
+				{"a..bc", "https://s3.mock-region.amazonaws.com/a..bc", ""},
+			},
+		},
+	}
 
-		if test.err != "" {
-			if err == nil {
-				t.Fatalf("test %d: expected error, got none", i)
-			}
-			if a, e := err.Error(), test.err; !strings.Contains(a, e) {
-				t.Fatalf("%d, expect error code to contain %q, got %q", i, e, a)
-			}
+	for name, c := range cases {
+		options := s3.Options{
+			Credentials: aws.StaticCredentialsProvider{
+				Value: aws.Credentials{
+					AccessKeyID: "AKID", SecretAccessKey: "SECRET", SessionToken: "SESSION",
+					Source: "unit test credentials",
+				}},
+			Retryer:      aws.NoOpRetryer{},
+			Region:       "mock-region",
+			UsePathStyle: c.usePathStyle,
 		}
 
-		req := fm.request
+		svc := s3.New(options)
+		for i, test := range c.tests {
+			fm := requestRetrieverMiddleware{}
+			_, err := svc.ListObjects(context.Background(),
+				&s3.ListObjectsInput{Bucket: &test.bucket},
+				func(options *s3.Options) {
+					options.APIOptions = append(options.APIOptions, func(stack *middleware.Stack) error {
+						stack.Serialize.Insert(&fm, "OperationSerializer", middleware.Before)
+						return nil
+					})
+				},
+			)
 
-		if e, a := test.url, req.URL.String(); e != a {
-			t.Fatalf("%d, expect url %s, got %s", i, e, a)
+			if test.err != "" {
+				if err == nil {
+					t.Fatalf("test %d: expected error, got none", i)
+				}
+				if a, e := err.Error(), test.err; !strings.Contains(a, e) {
+					t.Fatalf("%s: %d, expect error code to contain %q, got %q", name, i, e, a)
+				}
+			}
+
+			req := fm.request
+
+			if e, a := test.url, req.URL.String(); e != a {
+				t.Fatalf("%s: %d, expect url %s, got %s", name, i, e, a)
+			}
 		}
-
 	}
 }
 
