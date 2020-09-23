9e2dd49a54cabc2b899166b60c848f35fb084dcd
JordonPhillips <phjordon@amazon.com>
2020-09-23 11:19:34 -0700

    Add explicit check for seekable glacier body

--- service/glacier/internal/customizations/treehash_test.go
+++ service/glacier/internal/customizations/treehash_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/awslabs/smithy-go/middleware"
 	smithyhttp "github.com/awslabs/smithy-go/transport/http"
 	"io"
+	"strings"
 	"testing"
 )
 
@@ -13,9 +14,11 @@ func TestTreeHashMiddleware(t *testing.T) {
 	m := TreeHashMiddleware{}
 
 	cases := map[string]struct {
-		Body   io.ReadSeeker
-		Linear string
-		Tree   string
+		Body       io.Reader
+		SetRequest func(r *smithyhttp.Request) *smithyhttp.Request
+		Linear     string
+		Tree       string
+		ExpectErr  string
 	}{
 		"all zeroes": {
 			Body: func() io.ReadSeeker {
@@ -28,6 +31,34 @@ func TestTreeHashMiddleware(t *testing.T) {
 			Linear: "68aff0c5a91aa0491752bfb96e3fef33eb74953804f6a2f7b708d5bcefa8ff6b",
 			Tree:   "154e26c78fd74d0c2c9b3cc4644191619dc4f2cd539ae2a74d5fd07957a3ee6a",
 		},
+		"non-seekable stream without precomputed hash": {
+			Body: func() io.Reader {
+				buf := make([]byte, 5767168)
+				for i := range buf {
+					buf[i] = '0'
+				}
+				return bytes.NewBuffer(buf)
+			}(),
+			ExpectErr: "tree hash",
+		},
+		"non-seekable stream with precomputed hash": {
+			Body: func() io.Reader {
+				buf := make([]byte, 5767168)
+				for i := range buf {
+					buf[i] = '0'
+				}
+				return bytes.NewBuffer(buf)
+			}(),
+			SetRequest: func(r *smithyhttp.Request) *smithyhttp.Request {
+				r.Header.Set("X-Amz-Content-Sha256", "precomputed")
+				r.Header.Set("X-Amz-Sha256-Tree-Hash", "precomputed")
+				return r
+			},
+			// These values aren't actually correct, but they are what we're explicitly setting.
+			// If the middleware were to try to compute the values anyway, the checks would fail.
+			Linear: "precomputed",
+			Tree:   "precomputed",
+		},
 	}
 
 	for name, c := range cases {
@@ -39,6 +70,9 @@ func TestTreeHashMiddleware(t *testing.T) {
 						req, ok := r.(*smithyhttp.Request)
 						if !ok || req == nil {
 							t.Fatalf("expect smithy request, got %T", r)
+						}
+						if c.SetRequest != nil {
+							req = c.SetRequest(req)
 						}
 						req, err := req.SetStream(c.Body)
 						if err != nil {
@@ -66,16 +100,27 @@ func TestTreeHashMiddleware(t *testing.T) {
 							t.Fatalf("expected tree hash to be \"%s\" but was \"%s\"", c.Tree, actualTree)
 						}
 
-						n, _ := c.Body.Seek(0, io.SeekCurrent)
-						if n != 0 {
-							t.Fatalf("expected body to be rewound, but was at position %d", n)
-						}
+						if body, ok := c.Body.(io.ReadSeeker); ok {
+							n, _ := body.Seek(0, io.SeekCurrent)
+							if n != 0 {
+								t.Fatalf("expected body to be rewound, but was at position %d", n)
+							}
 
+						}
 						return output, metadata, err
 					}),
 			)
-			if err != nil {
-				t.Fatalf("expect no error, got %v", err)
+			if len(c.ExpectErr) != 0 {
+				if err == nil {
+					t.Fatalf("expect error, got none")
+				}
+				if e, a := c.ExpectErr, err.Error(); !strings.Contains(a, e) {
+					t.Fatalf("expect error to contain %v, got %v", e, a)
+				}
+			} else {
+				if err != nil {
+					t.Fatalf("expect no error, got %v", err)
+				}
 			}
 		})
 	}
