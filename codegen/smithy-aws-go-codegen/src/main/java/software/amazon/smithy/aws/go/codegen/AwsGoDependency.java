66bc19272ffceb0068c9f93c74829f784792b48a
Sean McGrail <mcgrails@amazon.com>
2020-09-24 13:03:34 -0700

    Update Dependency Version

--- codegen/smithy-aws-go-codegen/src/main/java/software/amazon/smithy/aws/go/codegen/AwsGoDependency.java
+++ codegen/smithy-aws-go-codegen/src/main/java/software/amazon/smithy/aws/go/codegen/AwsGoDependency.java
@@ -53,6 +53,6 @@ public class AwsGoDependency {
     }
 
     private static final class Versions {
-        private static final String AWS_SDK = "v0.0.0-20200924165317-475229a2f000";
+        private static final String AWS_SDK = "v0.24.1-0.20200924195609-61ca34a7860e";
     }
 }
