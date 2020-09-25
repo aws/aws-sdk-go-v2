29fb95e93e93a1fb9b011be3a957384a0bcbfafc
Jason Del Ponte <delpontej@gmail.com>
2020-09-25 12:48:56 -0700

    add debug request logging option

--- internal/integrationtest/config.go
+++ internal/integrationtest/config.go
@@ -1,14 +1,34 @@
 package integrationtest
 
 import (
+	"log"
+	"os"
+	"strings"
+
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	smithyhttp "github.com/awslabs/smithy-go/transport/http"
 )
 
 // LoadConfigWithDefaultRegion loads the default configuration for the SDK, and
 // falls back to a default region if one is not specified.
 func LoadConfigWithDefaultRegion(defaultRegion string) (cfg aws.Config, err error) {
-	cfg, err = config.LoadDefaultConfig()
+	var opts []config.Config
+
+	if strings.EqualFold(os.Getenv("AWS_DEBUG_REQUEST"), "true") {
+		opts = append(opts, config.WithHTTPClient{
+			HTTPClient: smithyhttp.WrapLogClient(
+				logger{}, aws.NewBuildableHTTPClient(), false),
+		})
+
+	} else if strings.EqualFold(os.Getenv("AWS_DEBUG_REQUEST_BODY"), "true") {
+		opts = append(opts, config.WithHTTPClient{
+			HTTPClient: smithyhttp.WrapLogClient(
+				logger{}, aws.NewBuildableHTTPClient(), true),
+		})
+	}
+
+	cfg, err = config.LoadDefaultConfig(opts...)
 	if err != nil {
 		return cfg, err
 	}
@@ -21,4 +41,10 @@ func LoadConfigWithDefaultRegion(defaultRegion string) (cfg aws.Config, err erro
 		RemoveOperationInputValidationMiddleware)
 
 	return cfg, nil
+}
+
+type logger struct{}
+
+func (logger) Logf(format string, args ...interface{}) {
+	log.Printf(format, args...)
 }
