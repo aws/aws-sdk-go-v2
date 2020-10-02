3bcd7f0ddf16720aad711c792c79e8b34a5adc11
Sean McGrail <mcgrails@amazon.com>
2020-10-01 17:18:27 -0700

    Update Endpoint and IMDS client to pass explicit retry middleware options.

--- ec2imds/request_middleware.go
+++ ec2imds/request_middleware.go
@@ -86,7 +86,7 @@ func addRequestMiddleware(stack *middleware.Stack,
 	}
 
 	// Retry support
-	retry.AddRetryMiddlewares(stack, options)
+	retry.AddRetryMiddlewares(stack, options.Retryer)
 
 	return nil
 }
