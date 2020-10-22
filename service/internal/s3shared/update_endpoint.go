4451b4af620e519105c4ca1d93162f5b6673d275
skotambkar <kotambka@amazon.com>
2020-10-22 08:12:03 -0700

    golint fix

--- service/internal/s3shared/update_endpoint.go
+++ service/internal/s3shared/update_endpoint.go
@@ -9,14 +9,20 @@ import (
 	"github.com/awslabs/smithy-go/transport/http"
 )
 
+// EnableDualstackMiddleware represents middleware struct for enabling dualstack support
 type EnableDualstackMiddleware struct {
+	// UseDualstack indicates if dualstack endpoint resolving is to be enabled
 	UseDualstack bool
-	ServiceID    string
+
+	// ServiceID is the service id prefix used in endpoint resolving
+	// by default service-id is 's3' and 's3-control' for service s3, s3control.
+	ServiceID string
 }
 
 // ID returns the middleware ID.
 func (*EnableDualstackMiddleware) ID() string { return "EnableDualstackMiddleware" }
 
+// HandleSerialize handles serializer middleware behavior when middleware is executed
 func (u *EnableDualstackMiddleware) HandleSerialize(
 	ctx context.Context, in middleware.SerializeInput, next middleware.SerializeHandler,
 ) (
