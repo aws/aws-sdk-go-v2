9d21a7d64e7216e79ab9f9018b2f231994e80ec8
skotambkar <kotambka@amazon.com>
2020-11-03 01:47:42 -0800

    cleanup

--- service/internal/integrationtest/s3/endpoint_test.go
+++ service/internal/integrationtest/s3/endpoint_test.go
@@ -9,10 +9,3 @@ import (
 func TestInteg_AccessPoint_WriteToObject(t *testing.T) {
 	testWriteToObject(t, setupMetadata.AccessPoints.Source.ARN, nil)
 }
-
-func TestInteg_AccessPoint_CopyObject(t *testing.T) {
-	t.Skip("API does not support access point")
-	testCopyObject(t,
-		setupMetadata.AccessPoints.Source.ARN,
-		setupMetadata.AccessPoints.Target.ARN, nil)
-}
