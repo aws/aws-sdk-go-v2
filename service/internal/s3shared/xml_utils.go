42835f7398e0cc67b5057e2d99877465bd09d769
skotambkar <kotambka@amazon.com>
2020-11-03 01:19:36 -0800

    fix s3 control error deserializer as it follows different pattern than s3

--- service/internal/s3shared/xml_utils.go
+++ service/internal/s3shared/xml_utils.go
@@ -17,8 +17,8 @@ type ErrorComponents struct {
 	HostID    string `xml:"HostId"`
 }
 
-// GetErrorResponseComponents returns the error fields from an xml error response body
-func GetErrorResponseComponents(r io.Reader) (ErrorComponents, error) {
+// GetUnwrappedErrorResponseComponents returns the error fields from an xml error response body
+func GetUnwrappedErrorResponseComponents(r io.Reader) (ErrorComponents, error) {
 	var errComponents ErrorComponents
 	if err := xml.NewDecoder(r).Decode(&errComponents); err != nil && err != io.EOF {
 		return ErrorComponents{}, fmt.Errorf("error while deserializing xml error response : %w", err)
@@ -26,20 +26,63 @@ func GetErrorResponseComponents(r io.Reader) (ErrorComponents, error) {
 	return errComponents, nil
 }
 
-// GetS3ErrorResponseComponents returns the error fields from an S3 xml error response body.
-// If an error code or message is not retrieved, it is derived from the http status code
-func GetS3ErrorResponseComponents(r io.Reader, statusCode int) (ErrorComponents, error) {
-	errComponents, err := GetErrorResponseComponents(r)
+// GetErrorResponseComponents returns the error fields from an xml error response body
+// in which error code, and message are wrapped by a <Error> tag
+func GetWrappedErrorResponseComponents(r io.Reader) (ErrorComponents, error) {
+	var errComponents struct {
+		Code      string `xml:"Error>Code"`
+		Message   string `xml:"Error>Message"`
+		RequestID string `xml:"RequestId"`
+		HostID    string `xml:"HostId"`
+	}
+
+	if err := xml.NewDecoder(r).Decode(&errComponents); err != nil && err != io.EOF {
+		return ErrorComponents{}, fmt.Errorf("error while deserializing xml error response : %w", err)
+	}
+
+	return ErrorComponents{
+		Code:      errComponents.Code,
+		Message:   errComponents.Message,
+		RequestID: errComponents.RequestID,
+		HostID:    errComponents.HostID,
+	}, nil
+}
+
+// GetErrorResponseComponents retrieves error components according to passed in options
+func GetErrorResponseComponents(r io.Reader, options ErrorResponseDeserializerOptions) (ErrorComponents, error) {
+	var errComponents ErrorComponents
+	var err error
+
+	if options.IsWrappedWithErrorTag {
+		errComponents, err = GetWrappedErrorResponseComponents(r)
+	} else {
+		errComponents, err = GetUnwrappedErrorResponseComponents(r)
+	}
+
 	if err != nil {
 		return ErrorComponents{}, err
 	}
 
-	// for S3 service, we derive err code and message, if none is found
-	if len(errComponents.Code) == 0 && len(errComponents.Message) == 0 {
+	// If an error code or message is not retrieved, it is derived from the http status code
+	// eg, for S3 service, we derive err code and message, if none is found
+	if options.UseStatusCode && len(errComponents.Code) == 0 &&
+		len(errComponents.Message) == 0 {
 		// derive code and message from status code
-		statusText := http.StatusText(statusCode)
+		statusText := http.StatusText(options.StatusCode)
 		errComponents.Code = strings.Replace(statusText, " ", "", -1)
 		errComponents.Message = statusText
 	}
 	return errComponents, nil
+}
+
+type ErrorResponseDeserializerOptions struct {
+	// UseStatusCode denotes if status code should be used to retrieve error code, msg
+	UseStatusCode bool
+
+	// StatusCode is status code of error response
+	StatusCode int
+
+	//IsWrappedWithErrorTag represents if error response's code, msg is wrapped within an
+	// additional <Error> tag
+	IsWrappedWithErrorTag bool
 }
