2d786000cceeb48b5079bb81e0103eb6f9a567d0
Jason Del Ponte <delpontej@gmail.com>
2020-11-12 15:17:30 -0800

    update gohash

--- codegen/smithy-aws-go-codegen/src/main/java/software/amazon/smithy/aws/go/codegen/AwsGoDependency.java
+++ codegen/smithy-aws-go-codegen/src/main/java/software/amazon/smithy/aws/go/codegen/AwsGoDependency.java
@@ -82,6 +82,6 @@ public class AwsGoDependency {
     }
 
     private static final class Versions {
-        private static final String AWS_SDK = "v0.29.0";
+        private static final String AWS_SDK = "v0.29.1-0.20201112231636-9ae467d8157d";
     }
 }
