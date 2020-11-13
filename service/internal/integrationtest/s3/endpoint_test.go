b322e8c623356bd98abf3b3b7c9023451adcb495
skotambkar <kotambka@amazon.com>
2020-11-13 14:58:15 -0800

    allow s3 accesspoint test to run

--- service/internal/integrationtest/s3/endpoint_test.go
+++ service/internal/integrationtest/s3/endpoint_test.go
@@ -7,6 +7,5 @@ import (
 )
 
 func TestInteg_AccessPoint_WriteToObject(t *testing.T) {
-	t.Skip("skip till accesspoint support is merged in")
 	testWriteToObject(t, setupMetadata.AccessPoints.Source.ARN, nil)
 }
