bdb4dbf91068ef73e661ba079aed39029da2be3a
Gavin Fitzgerald <gavinf@uneeq.com>
2021-03-25 15:38:39 +1300

    better comment description

--- internal/ini/literal_tokens.go
+++ internal/ini/literal_tokens.go
@@ -203,7 +203,7 @@ func NewIntValue(i int64) (Value, error) {
 	return newValue(IntegerType, 10, []rune{rune(i)})
 }
 
-// NewRawIntValue returns a Value type generated using an int64 input.
+// NewRawIntValue returns an Integer Value type generated using a rune input.
 func NewRawIntValue(i []rune) (Value, error) {
 	return newValue(IntegerType, 10, i)
 }
