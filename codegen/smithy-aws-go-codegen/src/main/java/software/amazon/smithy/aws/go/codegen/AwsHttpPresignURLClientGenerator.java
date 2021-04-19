0c6d3a4266b45a5bf5fafb5217905e73d0133cba
Francis Chuang <francis.chuang@boostport.com>
2021-04-19 12:42:19 +1000

    Implement presign for s3 UploadPart

--- codegen/smithy-aws-go-codegen/src/main/java/software/amazon/smithy/aws/go/codegen/AwsHttpPresignURLClientGenerator.java
+++ codegen/smithy-aws-go-codegen/src/main/java/software/amazon/smithy/aws/go/codegen/AwsHttpPresignURLClientGenerator.java
@@ -83,7 +83,8 @@ public class AwsHttpPresignURLClientGenerator implements GoIntegration {
     private static final Map<ShapeId, Set<ShapeId>> presignedClientMap = MapUtils.of(
             ShapeId.from("com.amazonaws.s3#AmazonS3"), SetUtils.of(
                     ShapeId.from("com.amazonaws.s3#GetObject"),
-                    ShapeId.from("com.amazonaws.s3#PutObject")
+                    ShapeId.from("com.amazonaws.s3#PutObject"),
+                    ShapeId.from("com.amazonaws.s3#UploadPart")
             ),
             ShapeId.from("com.amazonaws.sts#AWSSecurityTokenServiceV20110615"), SetUtils.of(
                     ShapeId.from("com.amazonaws.sts#GetCallerIdentity"))
