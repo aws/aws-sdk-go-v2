f4c4e7ce374b9a6d4a91dfb85808ccef21bc7d4b
Jason Del Ponte <961963+jasdel@users.noreply.github.com>
2021-05-26 09:02:00 -0700

    remove deprecated service/commander module source (#1277)
    Removes deprecated service/commander module source.

--- service/commander/doc.go
+++
@@ -1,5 +0,0 @@
-// Package commander is a deprecated API client. See AWS Systems Manager
-// Incident Manager (ssmincidents) for the correct API client module.
-//
-// Deprecated: Use AWS Systems Manager Incident Manager (ssmincidents).
-package commander
