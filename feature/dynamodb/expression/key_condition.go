15c64fd0bb8076dffc403eb8b8dbc1faa1dc60de
Zaq? Wiedmann <zaq.wiedmann@outreach.io>
2021-07-01 13:59:55 -0700

    feat: (feature/dynamodb/expression) readd IsSet methods
    These changes were present in an older version of the expression package before it was moved, originally added in https://github.com/aws/aws-sdk-go-v2/pull/494, when the expression library was reintroduce in https://github.com/aws/aws-sdk-go-v2/pull/981 these changes werent included for some reason, but the need for checking if a condition is set or not still exists

--- feature/dynamodb/expression/key_condition.go
+++ feature/dynamodb/expression/key_condition.go
@@ -68,6 +68,20 @@ func KeyEqual(keyBuilder KeyBuilder, valueBuilder ValueBuilder) KeyConditionBuil
 	}
 }
 
+// IsSet returns true if the KeyConditionBuilder is set and returns
+// false otherwise. A zero value of a KeyConditionBuilder returns false.
+//
+// Example:
+//
+//     var keyCondition expression.KeyConditionBuilder
+//     keyCondition.IsSet() // returns false
+//
+//     keyCondition := expression.KeyEqual(expression.Key("foo"), expression.Value(5))
+//     keyCondition.IsSet() // returns true
+func (kcb KeyConditionBuilder) IsSet() bool {
+	return kcb.mode != unsetKeyCond
+}
+
 // Equal returns a KeyConditionBuilder representing the equality clause of
 // the two argument OperandBuilders. The resulting KeyConditionBuilder can be
 // used as a part of other Key Condition Expressions or as an argument to the
