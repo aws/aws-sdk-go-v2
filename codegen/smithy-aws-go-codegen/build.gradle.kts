e312f333fff04c6cd883965210a498fdd80edbdc
JordonPhillips <JordonPhillips@users.noreply.github.com>
2021-09-16 14:41:43 +0200

    Update gradle configs

--- codegen/smithy-aws-go-codegen/build.gradle.kts
+++ codegen/smithy-aws-go-codegen/build.gradle.kts
@@ -35,10 +35,10 @@ dependencies {
     api("software.amazon.smithy:smithy-aws-iam-traits:[1.6.1,2.0.0[")
     api("software.amazon.smithy:smithy-aws-cloudformation-traits:[1.8.0,2.0.0[")
     api("software.amazon.smithy.go:smithy-go-codegen:0.1.0")
-    testCompile("org.junit.jupiter:junit-jupiter-api:5.4.0")
-    testRuntime("org.junit.jupiter:junit-jupiter-engine:5.4.0")
-    testCompile("org.junit.jupiter:junit-jupiter-params:5.4.0")
-    testCompile("org.hamcrest:hamcrest:2.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.4.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.4.0")
+    testCompileOnly("org.junit.jupiter:junit-jupiter-params:5.4.0")
+    testImplementation("org.hamcrest:hamcrest:2.1")
 }
 
 // == Code coverage ==
