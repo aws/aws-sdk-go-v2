54c5dd79da51255301812dfabd37f90818a2316c
Shantanu Kotambkar <52007797+skotambkar@users.noreply.github.com>
2021-09-30 13:17:17 -0700

    update endpoint for cloud control (#1444)

--- service/cloudcontrol/internal/endpoints/endpoints.go
+++ service/cloudcontrol/internal/endpoints/endpoints.go
@@ -62,6 +62,59 @@ var defaultPartitions = endpoints.Partitions{
 		},
 		RegionRegex:    partitionRegexp.Aws,
 		IsRegionalized: true,
+		Endpoints: endpoints.Endpoints{
+			"af-south-1":     endpoints.Endpoint{},
+			"ap-east-1":      endpoints.Endpoint{},
+			"ap-northeast-1": endpoints.Endpoint{},
+			"ap-northeast-2": endpoints.Endpoint{},
+			"ap-northeast-3": endpoints.Endpoint{},
+			"ap-south-1":     endpoints.Endpoint{},
+			"ap-southeast-1": endpoints.Endpoint{},
+			"ap-southeast-2": endpoints.Endpoint{},
+			"ca-central-1":   endpoints.Endpoint{},
+			"eu-central-1":   endpoints.Endpoint{},
+			"eu-north-1":     endpoints.Endpoint{},
+			"eu-south-1":     endpoints.Endpoint{},
+			"eu-west-1":      endpoints.Endpoint{},
+			"eu-west-2":      endpoints.Endpoint{},
+			"eu-west-3":      endpoints.Endpoint{},
+			"fips-ca-central-1": endpoints.Endpoint{
+				Hostname: "cloudcontrolapi-fips.ca-central-1.amazonaws.com",
+				CredentialScope: endpoints.CredentialScope{
+					Region: "ca-central-1",
+				},
+			},
+			"fips-us-east-1": endpoints.Endpoint{
+				Hostname: "cloudcontrolapi-fips.us-east-1.amazonaws.com",
+				CredentialScope: endpoints.CredentialScope{
+					Region: "us-east-1",
+				},
+			},
+			"fips-us-east-2": endpoints.Endpoint{
+				Hostname: "cloudcontrolapi-fips.us-east-2.amazonaws.com",
+				CredentialScope: endpoints.CredentialScope{
+					Region: "us-east-2",
+				},
+			},
+			"fips-us-west-1": endpoints.Endpoint{
+				Hostname: "cloudcontrolapi-fips.us-west-1.amazonaws.com",
+				CredentialScope: endpoints.CredentialScope{
+					Region: "us-west-1",
+				},
+			},
+			"fips-us-west-2": endpoints.Endpoint{
+				Hostname: "cloudcontrolapi-fips.us-west-2.amazonaws.com",
+				CredentialScope: endpoints.CredentialScope{
+					Region: "us-west-2",
+				},
+			},
+			"me-south-1": endpoints.Endpoint{},
+			"sa-east-1":  endpoints.Endpoint{},
+			"us-east-1":  endpoints.Endpoint{},
+			"us-east-2":  endpoints.Endpoint{},
+			"us-west-1":  endpoints.Endpoint{},
+			"us-west-2":  endpoints.Endpoint{},
+		},
 	},
 	{
 		ID: "aws-cn",
@@ -102,5 +155,21 @@ var defaultPartitions = endpoints.Partitions{
 		},
 		RegionRegex:    partitionRegexp.AwsUsGov,
 		IsRegionalized: true,
+		Endpoints: endpoints.Endpoints{
+			"fips-us-gov-east-1": endpoints.Endpoint{
+				Hostname: "cloudcontrolapi-fips.us-gov-east-1.amazonaws.com",
+				CredentialScope: endpoints.CredentialScope{
+					Region: "us-gov-east-1",
+				},
+			},
+			"fips-us-gov-west-1": endpoints.Endpoint{
+				Hostname: "cloudcontrolapi-fips.us-gov-west-1.amazonaws.com",
+				CredentialScope: endpoints.CredentialScope{
+					Region: "us-gov-west-1",
+				},
+			},
+			"us-gov-east-1": endpoints.Endpoint{},
+			"us-gov-west-1": endpoints.Endpoint{},
+		},
 	},
 }
