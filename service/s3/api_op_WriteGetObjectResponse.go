31dc91f50d5a5d9b26c3e6a4215d9a4db3b26348
Shantanu Kotambkar <kotambka@amazon.com>
2021-10-15 08:04:59 +0000

    regenerated s3 client

--- service/s3/api_op_WriteGetObjectResponse.go
+++ service/s3/api_op_WriteGetObjectResponse.go
@@ -326,6 +326,9 @@ func (c *Client) addOperationWriteGetObjectResponseMiddlewares(stack *middleware
 	if err = addResponseErrorMiddleware(stack); err != nil {
 		return err
 	}
+	if err = v4.SwapPayloadSHA256ResolverMiddleware(stack); err != nil {
+		return err
+	}
 	if err = disableAcceptEncodingGzip(stack); err != nil {
 		return err
 	}
