64a1c9201d2db7687c4fc334b9900cf9480a586e
Jason Del Ponte <961963+jasdel@users.noreply.github.com>
2022-01-10 16:03:28 -0800

    remove unneeded dependency directive

--- codegen/protocol-test-codegen/build.gradle.kts
+++ codegen/protocol-test-codegen/build.gradle.kts
@@ -32,7 +32,6 @@ plugins {
 }
 
 dependencies {
-	implementation("software.amazon.smithy:smithy-cli:$smithyVersion")
     implementation("software.amazon.smithy:smithy-aws-protocol-tests:$smithyVersion")
     implementation(project(":smithy-aws-go-codegen"))
 }
