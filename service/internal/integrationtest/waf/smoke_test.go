66251136b55d3a64355b88883f226f1d24a40bc7
Sean McGrail <mcgrails@amazon.com>
2022-01-18 23:59:11 +0000

    Remove negative smoke test for WAF (#1564)

--- service/internal/integrationtest/waf/smoke_test.go
+++ service/internal/integrationtest/waf/smoke_test.go
@@ -5,15 +5,11 @@ package waf
 
 import (
 	"context"
-	"errors"
 	"testing"
 	"time"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/service/waf"
-	"github.com/aws/smithy-go"
-
 	"github.com/aws/aws-sdk-go-v2/service/internal/integrationtest"
+	"github.com/aws/aws-sdk-go-v2/service/waf"
 )
 
 func TestInteg_00_ListRules(t *testing.T) {
@@ -32,36 +28,5 @@ func TestInteg_00_ListRules(t *testing.T) {
 	_, err = client.ListRules(ctx, params)
 	if err != nil {
 		t.Errorf("expect no error, got %v", err)
-	}
-}
-
-func TestInteg_01_CreateSqlInjectionMatchSet(t *testing.T) {
-	ctx, cancelFn := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancelFn()
-
-	cfg, err := integrationtest.LoadConfigWithDefaultRegion("us-west-2")
-	if err != nil {
-		t.Fatalf("failed to load config, %v", err)
-	}
-
-	client := waf.NewFromConfig(cfg)
-	params := &waf.CreateSqlInjectionMatchSetInput{
-		ChangeToken: aws.String("fake_token"),
-		Name:        aws.String("fake_name"),
-	}
-	_, err = client.CreateSqlInjectionMatchSet(ctx, params)
-	if err == nil {
-		t.Fatalf("expect request to fail")
-	}
-
-	var apiErr smithy.APIError
-	if !errors.As(err, &apiErr) {
-		t.Fatalf("expect error to be API error, was not, %v", err)
-	}
-	if len(apiErr.ErrorCode()) == 0 {
-		t.Errorf("expect non-empty error code")
-	}
-	if len(apiErr.ErrorMessage()) == 0 {
-		t.Errorf("expect non-empty error message")
 	}
 }
