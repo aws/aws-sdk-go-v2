f4bfda36b6763492bcf29f5e19fed85053ff4512
Sean McGrail <mcgrails@amazon.com>
2022-02-01 23:41:09 +0000

    Replace timestreamwrite integration test (#1577)

--- service/internal/integrationtest/timestreamwrite/smoke_test.go
+++ service/internal/integrationtest/timestreamwrite/smoke_test.go
@@ -12,7 +12,7 @@ import (
 	"time"
 )
 
-func TestInteg_00_CreateDatabase(t *testing.T) {
+func TestInteg_00_ListDatabases(t *testing.T) {
 	ctx, cancelFn := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancelFn()
 
@@ -24,17 +24,9 @@ func TestInteg_00_CreateDatabase(t *testing.T) {
 	// Create an Amazon timestreamwrite service client
 	client := tw.NewFromConfig(cfg)
 
-	// CreateDatabase
-	output, err := client.CreateDatabase(ctx, &tw.CreateDatabaseInput{
-		DatabaseName: aws.String("testDB"),
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// DeleteDatabase
-	_, err = client.DeleteDatabase(ctx, &tw.DeleteDatabaseInput{
-		DatabaseName: output.Database.DatabaseName,
+	// ListDatabases
+	_, err = client.ListDatabases(ctx, &tw.ListDatabasesInput{
+		MaxResults: aws.Int32(1),
 	})
 	if err != nil {
 		t.Fatal(err)
