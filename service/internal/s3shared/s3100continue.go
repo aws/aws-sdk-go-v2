59d9ba09118d707f52e50984649b0985770f1955
Tianyi Wang <wty@amazon.com>
2023-03-15 18:17:41 -0400

    Modify some logic in s3 100-continue middleware PR

--- service/internal/s3shared/s3100continue.go
+++ service/internal/s3shared/s3100continue.go
@@ -8,14 +8,18 @@ import (
 )
 
 const s3100ContinueID = "S3100Continue"
+const defaultLimit int64 = 1024 * 1024 * 2
 
 // Add100Continue add middleware, which adds {Expect: 100-continue} header for s3 client HTTP PUT request larger than 2MB
 // or with unknown size streaming bodies, during operation builder step
-func Add100Continue(stack *middleware.Stack) error {
-	return stack.Build.Add(&s3100Continue{}, middleware.After)
+func Add100Continue(stack *middleware.Stack, option AddContinueOption) error {
+	return stack.Build.Add(&s3100Continue{
+		continueHeaderThresholdBytes: option.ContinueHeaderThresholdBytes,
+	}, middleware.After)
 }
 
 type s3100Continue struct {
+	continueHeaderThresholdBytes int64
 }
 
 // ID returns the middleware identifier
@@ -28,14 +32,27 @@ func (m *s3100Continue) HandleBuild(
 ) (
 	out middleware.BuildOutput, metadata middleware.Metadata, err error,
 ) {
+	sizeLimit := defaultLimit
+	switch {
+	case m.continueHeaderThresholdBytes == -1:
+		return next.HandleBuild(ctx, in)
+	case m.continueHeaderThresholdBytes > 0:
+		sizeLimit = m.continueHeaderThresholdBytes
+	default:
+	}
+
 	req, ok := in.Request.(*smithyhttp.Request)
 	if !ok {
 		return out, metadata, fmt.Errorf("unknown request type %T", req)
 	}
 
-	if req.Method == "PUT" && (req.ContentLength == -1 || (req.ContentLength == 0 && req.Body != nil) || req.ContentLength >= 1024*1024*2) {
+	if req.ContentLength == -1 || (req.ContentLength == 0 && req.Body != nil) || req.ContentLength >= sizeLimit {
 		req.Header.Set("Expect", "100-continue")
 	}
 
 	return next.HandleBuild(ctx, in)
+}
+
+type AddContinueOption struct {
+	ContinueHeaderThresholdBytes int64
 }
