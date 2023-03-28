241dfb27e7f5ecfa52a14f1f47aab76600d3125a
Isaiah Vita <isvita@amazon.com>
2023-03-28 22:05:05 +0000

    remove error collector for now

--- internal/endpoints/awsrulesfn/arn.go
+++ internal/endpoints/awsrulesfn/arn.go
@@ -1,9 +1,7 @@
 package awsrulesfn
 
 import (
-	"fmt"
 	"strings"
-	smithyrulesfn "github.com/aws/smithy-go/private/endpoints/rulesfn"
 )
 
 // ARN provides AWS ARN components broken out into a data structure.
@@ -32,47 +30,23 @@ const (
 // ParseARN returns an [ARN] value parsed from the input string provided. If
 // the ARN cannot be parsed nil will be returned, and error added to
 // [ErrorCollector].
-func ParseARN(input string, ec *smithyrulesfn.ErrorCollector) *ARN {
+func ParseARN(input string) *ARN {
 	if !strings.HasPrefix(input, arnPrefix) {
-		ec.AddError(smithyrulesfn.FnError{
-			Name: "ParseARN",
-			Err:  fmt.Errorf("invalid ARN prefix, %q", input),
-		})
 		return nil
 	}
 
 	sections := strings.SplitN(input, arnDelimiters, arnSections)
 	if numSections := len(sections); numSections != arnSections {
-		ec.AddError(smithyrulesfn.FnError{
-			Name: "ParseARN",
-			Err: fmt.Errorf("invalid ARN, not enough sections, %q, %d",
-				input, numSections),
-		})
 		return nil
 	}
 
 	if sections[sectionPartition] == "" {
-		ec.AddError(smithyrulesfn.FnError{
-			Name: "ParseARN",
-			Err: fmt.Errorf("invalid ARN, partition section cannot be empty, %v",
-				input),
-		})
 		return nil
 	}
 	if sections[sectionService] == "" {
-		ec.AddError(smithyrulesfn.FnError{
-			Name: "ParseARN",
-			Err: fmt.Errorf("invalid ARN, service section cannot be empty, %v",
-				input),
-		})
 		return nil
 	}
 	if sections[sectionResource] == "" {
-		ec.AddError(smithyrulesfn.FnError{
-			Name: "ParseARN",
-			Err: fmt.Errorf("invalid ARN, resource section cannot be empty, %v",
-				input),
-		})
 		return nil
 	}
 
