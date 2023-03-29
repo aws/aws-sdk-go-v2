c40b4dd1a9240fed708c691786f2f6158ea61bb5
Isaiah Vita <82135527+isaiahvita@users.noreply.github.com>
2023-03-29 08:44:49 -0700

    remove error collector for now (#2073)

--- internal/endpoints/awsrulesfn/host.go
+++ internal/endpoints/awsrulesfn/host.go
@@ -1,10 +1,8 @@
 package awsrulesfn
 
 import (
-	"fmt"
 	"net/netip"
 	"strings"
-	smithyrulesfn "github.com/aws/smithy-go/private/endpoints/rulesfn"
 	smithyhttp "github.com/aws/smithy-go/transport/http"
 
 )
@@ -14,13 +12,9 @@ import (
 // to [rulesfn.IsValidHostLabel] with the added restriction that the length of label
 // must be [3:63] characters long, all lowercase, and not formatted as an IP
 // address.
-func IsVirtualHostableS3Bucket(input string, allowSubDomains bool, ec *smithyrulesfn.ErrorCollector) bool {
+func IsVirtualHostableS3Bucket(input string, allowSubDomains bool) bool {
 	// input should not be formatted as an IP address
 	if _, err := netip.ParseAddr(input); err == nil {
-		ec.AddError(smithyrulesfn.FnError{
-			Name: "IsVirtualHostableS3Bucket",
-			Err:  fmt.Errorf("host label is formatted like IP address, %q", input),
-		})
 		return false
 	}
 
@@ -31,33 +25,21 @@ func IsVirtualHostableS3Bucket(input string, allowSubDomains bool, ec *smithyrul
 		labels = []string{input}
 	}
 
-	for i, label := range labels {
+	for _, label := range labels {
 		// validate special length constraints
 		if l := len(label); l < 3 || l > 63 {
-			ec.AddError(smithyrulesfn.FnError{
-				Name: "IsVirtualHostableS3Bucket",
-				Err:  fmt.Errorf("host label %d has invalid length, %q, %d", i, label, l),
-			})
 			return false
 		}
 
 		// Validate no capital letters
 		for _, r := range label {
 			if r >= 'A' && r <= 'Z' {
-				ec.AddError(smithyrulesfn.FnError{
-					Name: "IsVirtualHostableS3Bucket",
-					Err:  fmt.Errorf("host label %d cannot have capital letters, %q", i, label),
-				})
 				return false
 			}
 		}
 
 		// Validate valid host label
 		if !smithyhttp.ValidHostLabel(label) {
-			ec.AddError(smithyrulesfn.FnError{
-				Name: "IsVirtualHostableS3Bucket",
-				Err:  fmt.Errorf("host label %d is invalid, %q", i, label),
-			})
 			return false
 		}
 	}
