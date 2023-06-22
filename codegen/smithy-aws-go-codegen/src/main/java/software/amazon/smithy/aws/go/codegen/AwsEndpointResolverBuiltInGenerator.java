669a80169d77430e72f4a8543f3647aad71122ec
Isaiah Vita <isvita@amazon.com>
2023-06-22 10:46:44 -0700

    add docs

--- codegen/smithy-aws-go-codegen/src/main/java/software/amazon/smithy/aws/go/codegen/AwsEndpointResolverBuiltInGenerator.java
+++ codegen/smithy-aws-go-codegen/src/main/java/software/amazon/smithy/aws/go/codegen/AwsEndpointResolverBuiltInGenerator.java
@@ -1,6 +1,8 @@
 package software.amazon.smithy.aws.go.codegen;
 
 import static software.amazon.smithy.go.codegen.GoWriter.goTemplate;
+import static software.amazon.smithy.go.codegen.GoWriter.goDocTemplate;
+
 import static software.amazon.smithy.aws.go.codegen.EndpointGenerator.USE_FIPS_ENDPOINT_OPTION;
 import static software.amazon.smithy.aws.go.codegen.EndpointGenerator.DUAL_STACK_ENDPOINT_OPTION;
 
@@ -11,6 +13,7 @@ import software.amazon.smithy.go.codegen.GoSettings;
 import software.amazon.smithy.go.codegen.GoWriter;
 import software.amazon.smithy.go.codegen.SymbolUtils;
 import software.amazon.smithy.go.codegen.TriConsumer;
+import software.amazon.smithy.go.codegen.endpoints.EndpointParametersGenerator;
 import software.amazon.smithy.go.codegen.endpoints.EndpointResolutionGenerator;
 import software.amazon.smithy.go.codegen.integration.GoIntegration;
 import software.amazon.smithy.model.Model;
@@ -32,7 +35,6 @@ public class AwsEndpointResolverBuiltInGenerator implements GoIntegration {
     public static final String BUILTIN_RESOLVER_INTERFACE_TYPE = "BuiltInParameterResolver";
     public static final String BUILTIN_RESOLVER_IMPLEMENTATION_TYPE = "BuiltInResolver";
 
-
     private Map<String, Object> commonCodegenArgs;
 
     @Override
@@ -42,6 +44,13 @@ public class AwsEndpointResolverBuiltInGenerator implements GoIntegration {
             SymbolProvider symbolProvider,
             TriConsumer<String, String, Consumer<GoWriter>> writerFactory
     ) {
+
+        this.commonCodegenArgs = MapUtils.of(
+            "resolverInterface", SymbolUtils.createValueSymbolBuilder(BUILTIN_RESOLVER_INTERFACE_TYPE).build(),
+            "resolverImpl", SymbolUtils.createValueSymbolBuilder(BUILTIN_RESOLVER_IMPLEMENTATION_TYPE).build(),
+            "endpointParametersType", EndpointResolutionGenerator.PARAMETERS_TYPE_NAME
+            );
+
         var serviceShape = settings.getService(model);
 
 
@@ -71,93 +80,80 @@ public class AwsEndpointResolverBuiltInGenerator implements GoIntegration {
     }
 
     private GoWriter.Writable generateBuiltInInterfaceType() {
-        return (GoWriter writer) -> {
-            writer.write(
-                """
-                    type $T interface {
-                        ResolveBuiltIns(*EndpointParameters) error
-                    }
-                """,
-                SymbolUtils.createValueSymbolBuilder(BUILTIN_RESOLVER_INTERFACE_TYPE).build()
-            );
-        };
+        return goTemplate(
+            """
+                $interfaceDoc:W
+                type $resolverInterface:T interface {
+                    ResolveBuiltIns(*$endpointParametersType:L) error
+                }
+            """,
+            commonCodegenArgs,
+            MapUtils.of(
+                "interfaceDoc", goDocTemplate(
+                    """
+                    $resolverInterface:T is the interface responsible for resolving BuiltIn values during the sourcing of $endpointParametersType:L
+                    """,
+                    commonCodegenArgs
+                )
+            )
+        );
     }
 
-    // private GoWriter.Writable generateBuiltInResolverEntryPoint(Parameters parameters) {
-    //     return (GoWriter w) -> {
-    //         w.write(
-    //             """
-    //             func resolveBuiltIns(parameters $P, resolver $T) {
-    //                 var value interface{}; var present bool
-    //                 $W
-    //             }
-    //             """,
-    //             SymbolUtils.createValueSymbolBuilder(EndpointResolutionGenerator.PARAMETERS_TYPE_NAME).build(),
-    //             SymbolUtils.createValueSymbolBuilder(BUILTIN_RESOLVER_INTERFACE_TYPE).build(),
-    //             generateBuiltInResolutionInvocation(parameters)
-    //         );
-    //     };
-    // }
-
-    // private GoWriter.Writable generateBuiltInResolutionInvocation(Parameters parameters) {
-    //     return (GoWriter w) -> {
-    //         parameters.toList().stream().filter(
-    //             p -> p.getBuiltIn().isPresent())
-    //             .forEach(parameter -> {
-    //                 w.write(
-    //                     """
-    //                         value, present = resolver.ResolveBuiltIn(\"$L\")
-    //                         if v, ok := value.($T); present && ok {
-    //                             parameters.$L = &v
-    //                         }
-    //                     """,
-    //                     parameter.getBuiltIn(),
-    //                     parameterAsSymbol(parameter),
-    //                     getExportedParameterName(parameter)
-    //                 );
-    //                 w.write("");
-    //         });
-    //     };
-    // }
-
     private GoWriter.Writable generateBuiltInImplementationType(Parameters parameters) {
-        return (GoWriter writer) -> {
-            writer.write(
-                """
-                type $T struct {
-                    $W
+        return goTemplate(
+            """
+                $structDoc:W
+                type $resolverImpl:T struct {
+                    $members:W
                 }
-                """,
-                SymbolUtils.createValueSymbolBuilder(BUILTIN_RESOLVER_IMPLEMENTATION_TYPE).build(),
-                generateBuiltInResolverMembers(parameters)
-            );
-        };
+            """,
+            commonCodegenArgs,
+            MapUtils.of(
+                "structDoc", goDocTemplate(
+                    """
+                    $resolverImpl:T resolves modeled BuiltIn values using only the members defined below. 
+                    """,
+                    commonCodegenArgs
+                ),
+                "members", generateBuiltInResolverMembers(parameters)
+            )
+        );
     }
 
     private GoWriter.Writable generateBuiltInResolverMembers(Parameters parameters) {
         return (GoWriter w) -> {
             parameters.toList().stream().filter(
                 p -> p.getBuiltIn().isPresent())
-                .forEach(parameter -> {
-                    if (parameter.getBuiltIn().get().equals("SDK::Endpoint")) {
-                        w.write("$L $P",
-                            getExportedParameterName(parameter),
-                            parameterAsSymbol(parameter));
-                    } else if (parameter.getBuiltIn().get().equals("AWS::UseFIPS")) {
-                        w.write("$L $T",
-                            getExportedParameterName(parameter),
-                            SymbolUtils.createValueSymbolBuilder("FIPSEndpointState", AwsGoDependency.AWS_CORE).build());
-                    } else if (parameter.getBuiltIn().get().equals("AWS::UseDualStack")) {
-                        w.write("$L $T",
-                            getExportedParameterName(parameter),
-                            SymbolUtils.createValueSymbolBuilder("DualStackEndpointState", AwsGoDependency.AWS_CORE).build());
-                    } else {
-                        w.write("$L $T",
-                            getExportedParameterName(parameter),
-                            parameterAsSymbol(parameter));
+                    .forEach(parameter -> {
+                        String template =
+                            """
+                            $W
+                            $L $T
+                            """;
+                        GoWriter.Writable docs;
+                        Symbol paramSymbol;
+                        if (parameter.getBuiltIn().get().equals("SDK::Endpoint")) {
+                            template =
+                                """
+                                $W
+                                $L $P
+                                """;
+                            docs = goDocTemplate("Base endpoint that can potentially be modified during Endpoint resolution.");
+                            paramSymbol = parameterAsSymbol(parameter);
+                        } else if (parameter.getBuiltIn().get().equals("AWS::UseFIPS")) {
+                            docs = goDocTemplate("Sourced BuiltIn value in a historical enabled or disabled state.");
+                            paramSymbol = SymbolUtils.createValueSymbolBuilder("FIPSEndpointState", AwsGoDependency.AWS_CORE).build();
+                        } else if (parameter.getBuiltIn().get().equals("AWS::UseDualStack")) {
+                            docs = goDocTemplate("Sourced BuiltIn value in a historical enabled or disabled state.");
+                            paramSymbol = SymbolUtils.createValueSymbolBuilder("DualStackEndpointState", AwsGoDependency.AWS_CORE).build();
+                        } else {
+                            docs = parameter.getDocumentation().isPresent() ? goDocTemplate(parameter.getDocumentation().get()) : goDocTemplate("");
+                            paramSymbol = parameterAsSymbol(parameter);
+                        }
+                        w.write(template, docs, getExportedParameterName(parameter), paramSymbol);
+                        w.insertTrailingNewline();
                     }
-                    w.insertTrailingNewline();
-            });
+                );
         };
     }
 
@@ -166,8 +162,15 @@ public class AwsEndpointResolverBuiltInGenerator implements GoIntegration {
         return (GoWriter writer) -> {
             writer.write(
                 """
-                func (b *BuiltInResolver) ResolveBuiltIns(params *EndpointParameters) error {
-                """
+                $W
+                func (b *BuiltInResolver) ResolveBuiltIns(params *$L) error {
+                """,
+                goDocTemplate(
+                    """
+                    Invoked at runtime to resolve BuiltIn Values. Only resolution code specific to each BuiltIn value is generated.       
+                    """
+                ),
+                EndpointResolutionGenerator.PARAMETERS_TYPE_NAME
             );
 
             parameters.toList().stream().filter(
@@ -286,53 +289,6 @@ public class AwsEndpointResolverBuiltInGenerator implements GoIntegration {
         };
     }
 
-
-
-
-    // private GoWriter.Writable generatePseudoRegionUtility() {
-    //     return (GoWriter writer) -> {
-    //         writer.write(
-    //             """
-    //             func mapPseudoRegion(pr string) (region string, fips aws.FIPSEndpointState) {
-    //                 const fipsInfix = \"-fips-\"
-    //                 const fipsPrefix = \"fips-\"
-    //                 const fipsSuffix = \"-fips\"
-                
-    //                 if strings.Contains(pr, fipsInfix) ||
-    //                     strings.Contains(pr, fipsPrefix) ||
-    //                     strings.Contains(pr, fipsSuffix) {
-    //                     region = strings.ReplaceAll(strings.ReplaceAll(strings.ReplaceAll(
-    //                         pr, fipsInfix, "-"), fipsPrefix, ""), fipsSuffix, "")
-    //                     fips = aws.FIPSEndpointStateEnabled
-    //                 } else {
-    //                     region = pr
-    //                 }
-                
-    //                 return region, fips
-    //             }
-    //             """
-    //         );
-    //     };
-    // }
-
-
-    // private GoWriter.Writable generateForcePathStyleMethod(Parameters parameters) {
-    //     return (GoWriter w) -> {
-    //         parameters.toList().stream().filter(
-    //             p -> p.getBuiltIn() == "AWS:S3::ForcePathStyle")
-    //             .forEach(parameter -> {
-    //                 w.write(
-    //                     """
-    //                     func (b *builtInResolver) resolveForcePathStyle() (value interface{}, ok bool) {
-
-    //                     }
-    //                     """
-
-    //             }
-
-    //         )
-    //     }
-    // }
 
     @Override
     public void renderEndpointBuiltInField(GoWriter writer) {
