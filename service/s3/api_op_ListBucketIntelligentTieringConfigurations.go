64120d5d98c678810efc76199f5f13bee8e90115
Steven Yuan <yuasteve@amazon.com>
2023-07-07 14:06:07 -0700

    Fix Properties Auth Scheme Path (#2177)
    Previously, properties were not being stored correctly, so the path for EP 2.0 Auth Scheme resolution was not being hit.
    
    After the fix from aws/smithy-go#434 was merged, issues happened in the new code path.

--- service/s3/api_op_ListBucketIntelligentTieringConfigurations.go
+++ service/s3/api_op_ListBucketIntelligentTieringConfigurations.go
@@ -6,6 +6,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"github.com/aws/aws-sdk-go-v2/aws"
 	awsmiddleware "github.com/aws/aws-sdk-go-v2/aws/middleware"
 	"github.com/aws/aws-sdk-go-v2/aws/signer/v4"
 	internalauth "github.com/aws/aws-sdk-go-v2/internal/auth"
@@ -325,9 +326,8 @@ func (m *opListBucketIntelligentTieringConfigurationsResolveEndpointMiddleware) 
 			break
 		case *internalauth.AuthenticationSchemeV4A:
 			v4aScheme, _ := authScheme.(*internalauth.AuthenticationSchemeV4A)
-			var signingName string
 			if v4aScheme.SigningName == nil {
-				signingName = "s3"
+				v4aScheme.SigningName = aws.String("s3")
 			}
 			if v4aScheme.DisableDoubleEncoding != nil {
 				// The signer sets an equivalent value at client initialization time.
@@ -335,7 +335,7 @@ func (m *opListBucketIntelligentTieringConfigurationsResolveEndpointMiddleware) 
 				// and override the value set at client initialization time.
 				ctx = internalauth.SetDisableDoubleEncoding(ctx, *v4aScheme.DisableDoubleEncoding)
 			}
-			ctx = awsmiddleware.SetSigningName(ctx, signingName)
+			ctx = awsmiddleware.SetSigningName(ctx, *v4aScheme.SigningName)
 			ctx = awsmiddleware.SetSigningRegion(ctx, v4aScheme.SigningRegionSet[0])
 			ctx = s3cust.SetSignerVersion(ctx, v4aScheme.Name)
 			break
