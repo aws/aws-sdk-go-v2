f61188c91bb4099350104855834838bd31c961ea
Isaiah Vita <isvita@amazon.com>
2023-07-24 09:54:26 -0700

    fix broken unit tests to use a mocked http client

--- service/s3/internal/customizations/unit_test.go
+++ service/s3/internal/customizations/unit_test.go
@@ -17,49 +17,47 @@ import (
 
 func Test_EmptyResponse(t *testing.T) {
 	cases := map[string]struct {
-		status       int
-		responseBody []byte
+		response       *http.Response
 		expectError  bool
 	}{
 		"success case with no response body": {
-			status:       200,
-			responseBody: []byte(``),
+			response: &http.Response{
+				StatusCode: 200,
+				Body: asReadCloser(
+					``,
+				),
+			},
 		},
 		"error case with no response body": {
-			status:       400,
-			responseBody: []byte(``),
+			response: &http.Response{
+				StatusCode: 400,
+				Body: asReadCloser(
+					``,
+				),
+			},
 			expectError:  true,
 		},
 	}
 
 	for name, c := range cases {
 		t.Run(name, func(t *testing.T) {
-			server := httptest.NewServer(http.HandlerFunc(
-				func(w http.ResponseWriter, r *http.Request) {
-					w.WriteHeader(c.status)
-					w.Write(c.responseBody)
-				}))
-			defer server.Close()
 
 			ctx, cancelFn := context.WithTimeout(context.Background(), 5*time.Second)
 			defer cancelFn()
 
 			cfg := aws.Config{
-				Region: "us-east-1",
-				EndpointResolver: aws.EndpointResolverFunc(func(service, region string) (aws.Endpoint, error) {
-					return aws.Endpoint{
-						URL:         server.URL,
-						SigningName: "s3",
-					}, nil
-				}),
+				Region: "mock-region",
 				Retryer: func() aws.Retryer {
 					return aws.NopRetryer{}
 				},
 			}
 
-			client := s3.NewFromConfig(cfg, func(options *s3.Options) {
-				options.UsePathStyle = true
-			})
+			client := s3.NewFromConfig(cfg,
+				func(options *s3.Options) {
+					options.UsePathStyle = true
+					options.HTTPClient = &mockHTTPClient{c.response}
+				},
+			)
 
 			params := &s3.HeadBucketInput{Bucket: aws.String("aws-sdk-go-data")}
 			_, err := client.HeadBucket(ctx, params)
