046abf83f283a02fb42ecb498c8af6c86262a116
AWS SDK for Go v2 automation user <aws-sdk-go-v2-github@amazon.com>
2023-10-06 18:17:55 +0000

    fix: fail to load config if configured profile doesn't exist

--- config/config.go
+++ config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"context"
+	"os"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 )
@@ -190,7 +191,7 @@ func LoadDefaultConfig(ctx context.Context, optFns ...func(*LoadOptions) error) 
 	// assign Load Options to configs
 	var cfgCpy = configs{options}
 
-	cfgCpy, err = cfgCpy.AppendFromLoaders(ctx, defaultLoaders)
+	cfgCpy, err = cfgCpy.AppendFromLoaders(ctx, resolveConfigLoaders(&options))
 	if err != nil {
 		return aws.Config{}, err
 	}
@@ -201,4 +202,18 @@ func LoadDefaultConfig(ctx context.Context, optFns ...func(*LoadOptions) error) 
 	}
 
 	return cfg, nil
+}
+
+func resolveConfigLoaders(options *LoadOptions) []loader {
+	loaders := make([]loader, 2)
+	loaders[0] = loadEnvConfig
+
+	// specification of a profile should cause a load failure if it doesn't exist
+	if os.Getenv(awsProfileEnvVar) != "" || options.SharedConfigProfile != "" {
+		loaders[1] = loadSharedConfig
+	} else {
+		loaders[1] = loadSharedConfigIgnoreNotExist
+	}
+
+	return loaders
 }
--- config/resolve_credentials_test.go
+++ config/resolve_credentials_test.go
@@ -577,8 +577,6 @@ func TestResolveCredentialsIMDSClient(t *testing.T) {
 			opts := []func(*LoadOptions) error{
 				WithRetryer(func() aws.Retryer { return aws.NopRetryer{} }),
 				WithHTTPClient(httpClient),
-				// separate from the local config, should it exist - the default loader will ignore nonexistent profiles
-				WithSharedConfigProfile(" "),
 				WithSharedConfigFiles([]string{}),
 			}
 
