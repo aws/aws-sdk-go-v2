7fcf22b772d810b32a29bad518d4e1517d11cf27
Isaiah Vita <82135527+isaiahvita@users.noreply.github.com>
2023-10-24 09:57:05 -0700

    Add internal endpoints config (#2335)

---
+++ internal/endpoints/config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"context"
+	"github.com/aws/aws-sdk-go-v2/config"
+)
+
+// ServiceBaseEndpointProvider is needed to search for all providers
+// that provide a configured service endpoint.
+type ServiceBaseEndpointProvider interface {
+	GetServiceBaseEndpoint(ctx context.Context, sdkID string) (string, bool, error)
+}
+
+// ResolveServiceBaseEndpoint is used to retrieve service endpoints from configured sources
+// while allowing for configured endpoints to be disabled.
+func ResolveServiceBaseEndpoint(ctx context.Context, sdkID string, configs []config.Config) (value string, found bool, err error) {
+	if val, found, _ := config.GetIgnoreConfiguredEndpoints(ctx, configs); found && val {
+		return "", false, nil
+	}
+
+	for _, cs := range configs {
+		if p, ok := cs.(ServiceBaseEndpointProvider); ok {
+			value, found, err = p.GetServiceBaseEndpoint(context.Background(), sdkID)
+			if err != nil || found {
+				break
+			}
+		}
+	}
+	return
+}
