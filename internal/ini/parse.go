712dd6ea1c2d26f6c80cd1149fd4ac80ad703386
Luc Talatinian <lucix@amazon.com>
2023-11-13 10:33:05 -0500

    allow leading whitespace on subproperties based on context

--- internal/ini/parse.go
+++ internal/ini/parse.go
@@ -2,6 +2,7 @@ package ini
 
 import (
 	"fmt"
+	"strings"
 )
 
 func parse(tokens []lineToken, path string) (Sections, error) {
@@ -24,39 +25,30 @@ func parse(tokens []lineToken, path string) (Sections, error) {
 				cfg.container[name] = NewSection(name)
 			}
 		case *lineTokenProperty:
-			if currSection == "" {
-				continue // LEGACY: don't error on "global" properties
-			}
-
-			currKey = tok.Key
-			if _, ok := cfg.container[currSection].values[tok.Key]; ok {
-				section := cfg.container[currSection]
-				section.Logs = append(cfg.container[currSection].Logs,
-					fmt.Sprintf(
-						"For profile: %v, overriding %v value, with a %v value found in a duplicate profile defined later in the same file %v. \n",
-						currSection, tok.Key, tok.Key, path,
-					),
-				)
-				cfg.container[currSection] = section
-			}
-
-			cfg.container[currSection].values[tok.Key] = Value{
-				str: tok.Value,
-			}
-			cfg.container[currSection].SourceFile[tok.Key] = path
-
+			handleProperty(&currSection, &currKey, &cfg, path, tok)
 		case *lineTokenSubProperty:
-			if currKey == "" {
+			if currSection == "" {
 				continue
 			}
 
-			value, _ := cfg.container[currSection].values[currKey]
-			if value.mp == nil && value.str == "" {
-				value.mp = map[string]string{
-					tok.Key: tok.Value,
+			if currKey == "" || cfg.container[currSection].values[currKey].str != "" {
+				// This is an "orphaned" subproperty, either because it's at
+				// the beginning of a section or because the last property's
+				// value isn't empty. Either way we're lenient here and
+				// "promote" this to a normal property.
+				handleProperty(&currSection, &currKey, &cfg, path, &lineTokenProperty{
+					Key:   tok.Key,
+					Value: strings.TrimSpace(trimComment(tok.Value)),
+				})
+				continue
+			}
+
+			if cfg.container[currSection].values[currKey].mp == nil {
+				cfg.container[currSection].values[currKey] = Value{
+					mp: map[string]string{},
 				}
 			}
-			cfg.container[currSection].values[currKey] = value
+			cfg.container[currSection].values[currKey].mp[tok.Key] = tok.Value
 		case *lineTokenContinuation:
 			if currKey == "" {
 				continue
@@ -71,4 +63,27 @@ func parse(tokens []lineToken, path string) (Sections, error) {
 		}
 	}
 	return cfg, nil
+}
+
+func handleProperty(currSection, currKey *string, cfg *Sections, path string, tok *lineTokenProperty) {
+	if *currSection == "" {
+		return // LEGACY: don't error on "global" properties
+	}
+
+	*currKey = tok.Key
+	if _, ok := cfg.container[*currSection].values[tok.Key]; ok {
+		section := cfg.container[*currSection]
+		section.Logs = append(cfg.container[*currSection].Logs,
+			fmt.Sprintf(
+				"For profile: %v, overriding %v value, with a %v value found in a duplicate profile defined later in the same file %v. \n",
+				*currSection, tok.Key, tok.Key, path,
+			),
+		)
+		cfg.container[*currSection] = section
+	}
+
+	cfg.container[*currSection].values[tok.Key] = Value{
+		str: tok.Value,
+	}
+	cfg.container[*currSection].SourceFile[tok.Key] = path
 }
