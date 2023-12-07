b3c7fbff96e191320f18e8513d20a73937d19675
Luc Talatinian <102624213+lucix-aws@users.noreply.github.com>
2023-12-07 16:30:12 -0500

    update express cache key (#2414)

--- service/s3/express_test.go
+++ service/s3/express_test.go
@@ -2,6 +2,7 @@ package s3
 
 import (
 	"context"
+	"net/http"
 	"sync"
 	"testing"
 	"time"
@@ -9,6 +10,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/internal/sdk"
 	"github.com/aws/aws-sdk-go-v2/service/s3/types"
+	"github.com/aws/smithy-go/middleware"
 )
 
 type mockCreateSession struct {
@@ -34,6 +36,22 @@ func (m *mockCreateSession) CreateSession(context.Context, *CreateSessionInput, 
 	o := m.calls[m.times]
 	m.times++
 	return o.output, o.err
+}
+
+type mockCreds struct {
+	akid, secret, session string
+}
+
+func newMockCreds(akid, secret, session string) *mockCreds {
+	return &mockCreds{akid: akid, secret: secret, session: session}
+}
+
+func (m *mockCreds) Retrieve(ctx context.Context) (aws.Credentials, error) {
+	return aws.Credentials{
+		AccessKeyID:     m.akid,
+		SecretAccessKey: m.secret,
+		SessionToken:    m.session,
+	}, nil
 }
 
 func TestS3Express_Retrieve(t *testing.T) {
@@ -67,6 +85,7 @@ func TestS3Express_Retrieve(t *testing.T) {
 
 	c := newDefaultS3ExpressCredentialsProvider()
 	c.client = mockClient
+	c.v4creds = newMockCreds("AKID", "SECRET", "SESSION")
 
 	mockClient.wg.Add(3)
 	c0, err := c.Retrieve(context.Background(), "bucket-0")
@@ -141,6 +160,7 @@ func TestS3Express_AsyncRefresh(t *testing.T) {
 
 	c := newDefaultS3ExpressCredentialsProvider()
 	c.client = mockClient
+	c.v4creds = newMockCreds("AKID", "SECRET", "SESSION")
 
 	mockClient.wg.Add(2)
 	c0, err := c.Retrieve(context.Background(), "bucket-0")
@@ -171,4 +191,120 @@ func TestS3Express_AsyncRefresh(t *testing.T) {
 	if expected != c1 {
 		t.Errorf("expected credentials %v, got %v", expected, c1)
 	}
+}
+
+type mockHTTP struct{}
+
+func (*mockHTTP) Do(*http.Request) (*http.Response, error) {
+	return &http.Response{}, nil
+}
+
+func TestS3Express_OperationCredentialOverride(t *testing.T) {
+	sdk.NowTime = func() time.Time {
+		return time.Unix(0, 0)
+	}
+
+	createSessionClient := &mockCreateSession{
+		calls: []mockCreateSessionCall{
+			{
+				output: &CreateSessionOutput{
+					Credentials: &types.SessionCredentials{
+						AccessKeyId:     aws.String("EXPRESS_AKID0"),
+						SecretAccessKey: aws.String("EXPRESS_SECRET0"),
+						SessionToken:    aws.String("EXPRESS_TOKEN0"),
+						Expiration:      aws.Time(time.Unix(3600, 0).UTC()),
+					},
+				},
+			},
+			{
+				output: &CreateSessionOutput{
+					Credentials: &types.SessionCredentials{
+						AccessKeyId:     aws.String("EXPRESS_AKID1"),
+						SecretAccessKey: aws.String("EXPRESS_SECRET1"),
+						SessionToken:    aws.String("EXPRESS_TOKEN1"),
+						Expiration:      aws.Time(time.Unix(3600, 0).UTC()),
+					},
+				},
+			},
+		},
+	}
+	createSessionClient.wg.Add(2)
+
+	svc := New(Options{
+		Region:      "us-west-2",
+		Credentials: newMockCreds("AKID0", "SECRET0", "SESSION0"),
+		HTTPClient:  &mockHTTP{},
+		APIOptions: []func(*middleware.Stack) error{
+			func(stack *middleware.Stack) error {
+				stack.Deserialize.Clear()
+				return stack.Deserialize.Add(
+					middleware.DeserializeMiddlewareFunc(
+						"mockResponse",
+						func(context.Context, middleware.DeserializeInput, middleware.DeserializeHandler) (middleware.DeserializeOutput, middleware.Metadata, error) {
+							out := middleware.DeserializeOutput{
+								Result: &GetObjectOutput{},
+							}
+							return out, middleware.Metadata{}, nil
+						},
+					),
+					middleware.After,
+				)
+			},
+		},
+	})
+
+	expressProvider, _ := svc.options.ExpressCredentials.(*defaultS3ExpressCredentialsProvider)
+	expressProvider.client = createSessionClient
+
+	_, err := svc.GetObject(context.Background(), &GetObjectInput{
+		Bucket: aws.String("bucket--usw2-az1--x-s3"),
+		Key:    aws.String("key"),
+	})
+	if err != nil {
+		t.Errorf("get object: %v", err)
+	}
+
+	// there should be one set of credentials in the cache
+	key0 := cacheKey{
+		CredentialsHash: gethmac("AKID0", "SECRET0"),
+		Bucket:          "bucket--usw2-az1--x-s3",
+	}
+	_, ok := expressProvider.cache.Get(key0)
+	if !ok {
+		t.Errorf("creds for AKID0/SECRET0 are missing")
+	}
+
+	_, err = svc.GetObject(context.Background(), &GetObjectInput{
+		Bucket: aws.String("bucket--usw2-az1--x-s3"),
+		Key:    aws.String("key"),
+	}, func(o *Options) {
+		o.Credentials = newMockCreds("AKID1", "SECRET1", "SESSION1")
+	})
+	if err != nil {
+		t.Errorf("get object: %v", err)
+	}
+
+	// checking two things here:
+	//   - we have a new cache entry since creds changed
+	//   - note we're still using the original pointer, the operation finalizer
+	//     should have copied it and passed the cache along
+	key1 := cacheKey{
+		CredentialsHash: gethmac("AKID1", "SECRET1"),
+		Bucket:          "bucket--usw2-az1--x-s3",
+	}
+	_, ok = expressProvider.cache.Get(key1)
+	if !ok {
+		t.Errorf("creds for AKID1/SECRET1 are missing")
+	}
+
+	// repeat of 1st call, should be a cache hit
+	_, err = svc.GetObject(context.Background(), &GetObjectInput{
+		Bucket: aws.String("bucket--usw2-az1--x-s3"),
+		Key:    aws.String("key"),
+	})
+	if err != nil {
+		t.Errorf("get object: %v", err)
+	}
+
+	createSessionClient.expectCalled(t, 2)
 }
