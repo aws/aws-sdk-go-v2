42bc3a8d2b7f7647d64b6454f8cf36e7f3c0ae68
Luc Talatinian <102624213+lucix-aws@users.noreply.github.com>
2024-03-20 17:19:47 -0400

    Update feature/ec2/imds/doc.go

--- feature/ec2/imds/doc.go
+++ feature/ec2/imds/doc.go
@@ -5,7 +5,7 @@
 // completed before this timeout expires, the operation will be canceled. This
 // timeout can be overridden through the following:
 //   - Set the options flag DisableDefaultTimeout
-//   - Provide a Context with a timeout or deadlin with calling the client's operations.
+//   - Provide a Context with a timeout or deadline with calling the client's operations.
 //
 // See the EC2 IMDS user guide for more information on using the API.
 // https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-instance-metadata.html
