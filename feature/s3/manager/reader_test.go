b89585211f0c21830a78057ddfddb6996da01498
Job Stoit <job@jobstoit.me>
2024-04-27 12:43:42 +0200

    test: add single part reader test

--- feature/s3/manager/reader_test.go
+++ feature/s3/manager/reader_test.go
@@ -11,7 +11,38 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
-func TestObjectReader(t *testing.T) {
+func TestObjectReaderSinglePart(t *testing.T) {
+	c, invocations, ranges := newDownloadRangeClient(buf2MB)
+
+	d := manager.NewDownloader(c, func(d *manager.Downloader) {
+		d.Concurrency = 1
+	})
+
+	rd := d.NewReader(context.Background(), &s3.GetObjectInput{
+		Bucket: aws.String("bucket"),
+		Key:    aws.String("key"),
+	})
+
+	n, err := io.Copy(io.Discard, rd)
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+
+	if e, a := int64(len(buf2MB)), n; e != a {
+		t.Errorf("expected %d buffer length, got %d", e, a)
+	}
+
+	if e, a := 2, *invocations; e != a {
+		t.Errorf("expect %v API calls, got %v", e, a)
+	}
+
+	expectRngs := []string{"bytes=0-0", "bytes=0-2097152"}
+	if e, a := expectRngs, *ranges; !reflect.DeepEqual(e, a) {
+		t.Errorf("expect %v ranges, got %v", e, a)
+	}
+}
+
+func TestObjectReaderMultiPart(t *testing.T) {
 	c, invocations, ranges := newDownloadRangeClient(buf12MB)
 
 	d := manager.NewDownloader(c, func(d *manager.Downloader) {
