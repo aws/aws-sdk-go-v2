b5be3ac11434ba84572d93d79003035ba8c573e4
Job Stoit <job@jobstoit.me>
2024-04-27 12:35:18 +0200

    test: add single part upload test

--- feature/s3/manager/writer_test.go
+++ feature/s3/manager/writer_test.go
@@ -11,7 +11,39 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
-func TestObjectWriter(t *testing.T) {
+func TestObjectWriterSingePartUpload(t *testing.T) {
+	s, ops, args := s3testing.NewUploadLoggingClient(nil)
+
+	mgr := manager.NewUploader(s, func(u *manager.Uploader) {
+		u.PartSize = 1024 * 1024 * 7
+		u.Concurrency = 1
+	})
+
+	wr := mgr.NewWriter(context.Background(), &s3.PutObjectInput{
+		Bucket: aws.String("bucket"),
+		Key:    aws.String("key"),
+	})
+
+	if _, err := wr.Write(buf2MB); err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	if err := wr.Close(); err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	vals := []string{"PutObject"}
+	if !reflect.DeepEqual(vals, *ops) {
+		t.Errorf("expect %v, got %v", vals, *ops)
+	}
+
+	// Part lengths
+	if e, a := int64(1024*1024*2), getReaderLength((*args)[0].(*s3.PutObjectInput).Body); e != a {
+		t.Errorf("expect %d, got %d", e, a)
+	}
+}
+
+func TestObjectWriterMultipartUpload(t *testing.T) {
 	s, ops, args := s3testing.NewUploadLoggingClient(nil)
 
 	mgr := manager.NewUploader(s, func(u *manager.Uploader) {
