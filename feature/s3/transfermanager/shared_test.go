78c2be4ca0b69cddb5825b20309e35d4f54c6cf4
Luc Talatinian <lucix@amazon.com>
2025-01-29 12:42:41 -0500

    Revert "beta: feature/s3/transfermanager (S3 transfer manager v2) (#2988)"
    This reverts commit 5c9d67c15ecc49b769f7ddab05f840e723e146ac.

--- feature/s3/transfermanager/shared_test.go
+++
@@ -1,4 +0,0 @@
-package transfermanager
-
-var buf20MB = make([]byte, 1024*1024*20)
-var buf2MB = make([]byte, 1024*1024*2)
--- service/internal/integrationtest/s3/setup_test.go
+++ service/internal/integrationtest/s3/setup_test.go
@@ -346,8 +346,8 @@ func testWriteToObject(t *testing.T, bucket string, testData writeToObjectTestDa
 		}
 
 	} else {
-		if e := testData.ExpectError; len(e) != 0 {
-			t.Fatalf("expected error: %v, got none", e)
+		if len(testData.ExpectError) != 0 {
+			t.Fatalf("expected error: %v, got none", err)
 		}
 	}
 
