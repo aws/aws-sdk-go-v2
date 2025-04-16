91234281ed4529f6029b1c740ed35a2b0077f9fb
Luc Talatinian <lucix@amazon.com>
2025-04-16 15:44:47 -0400

    implement progress listener events for PutObject

--- feature/s3/transfermanager/progress_listener_test.go
+++ feature/s3/transfermanager/progress_listener_test.go
@@ -2,17 +2,18 @@ package transfermanager
 
 import (
 	"context"
+	"sync"
 	"testing"
 )
 
-type mockListener struct{}
+type mockPartialListener struct{}
 
-func (mockListener) OnObjectTransferStart(context.Context, *ObjectTransferStartEvent)       {}
-func (mockListener) OnObjectTransferComplete(context.Context, *ObjectTransferCompleteEvent) {}
+func (mockPartialListener) OnObjectTransferStart(context.Context, *ObjectTransferStartEvent)       {}
+func (mockPartialListener) OnObjectTransferComplete(context.Context, *ObjectTransferCompleteEvent) {}
 
 func TestProgressListenerRegisterAndCopy(t *testing.T) {
 	o := Options{}
-	o.ProgressListeners.Register(mockListener{})
+	o.ProgressListeners.Register(mockPartialListener{})
 
 	expectIntEq(t, 1, len(o.ProgressListeners.ObjectTransferStart))
 	expectIntEq(t, 0, len(o.ProgressListeners.ObjectBytesTransferred))
@@ -20,7 +21,7 @@ func TestProgressListenerRegisterAndCopy(t *testing.T) {
 	expectIntEq(t, 0, len(o.ProgressListeners.ObjectTransferFailed))
 
 	cp := o.Copy()
-	cp.ProgressListeners.Register(mockListener{})
+	cp.ProgressListeners.Register(mockPartialListener{})
 
 	expectIntEq(t, 1, len(o.ProgressListeners.ObjectTransferStart))
 	expectIntEq(t, 0, len(o.ProgressListeners.ObjectBytesTransferred))
@@ -37,5 +38,114 @@ func expectIntEq(t *testing.T, expect, actual int) {
 	t.Helper()
 	if expect != actual {
 		t.Errorf("%v != %v", expect, actual)
+	}
+}
+
+type mockListener struct {
+	mu sync.Mutex
+
+	start    []*ObjectTransferStartEvent
+	transfer []*ObjectBytesTransferredEvent
+	complete []*ObjectTransferCompleteEvent
+	failed   []*ObjectTransferFailedEvent
+}
+
+func (m *mockListener) OnObjectTransferStart(ctx context.Context, event *ObjectTransferStartEvent) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	m.start = append(m.start, event)
+}
+
+func (m *mockListener) OnObjectBytesTransferred(ctx context.Context, event *ObjectBytesTransferredEvent) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	m.transfer = append(m.transfer, event)
+}
+
+func (m *mockListener) OnObjectTransferComplete(ctx context.Context, event *ObjectTransferCompleteEvent) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	m.complete = append(m.complete, event)
+}
+
+func (m *mockListener) OnObjectTransferFailed(ctx context.Context, event *ObjectTransferFailedEvent) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	m.failed = append(m.failed, event)
+}
+
+func (m *mockListener) expectComplete(t *testing.T, in, out any) {
+	t.Helper()
+
+	if len(m.start) != 1 {
+		t.Fatalf("transfer start was called %d times instead of once", len(m.start))
+	}
+	if len(m.complete) != 1 {
+		t.Fatalf("transfer complete was called %d times instead of once", len(m.complete))
+	}
+	if len(m.failed) != 0 {
+		t.Fatalf("transfer failed was called on expected completion: %v", m.failed[0].Error)
+	}
+
+	start := m.start[0]
+	complete := m.complete[0]
+
+	// input/output are all literal equality checks because what we emit in
+	// progress AND return should be the same pointer
+	if in != start.Input {
+		t.Errorf("transfer start: input %v != %v", in, start.Input)
+	}
+	if in != complete.Input {
+		t.Errorf("transfer complete: input %v != %v", in, complete.Input)
+	}
+	if out != complete.Output {
+		t.Errorf("transfer complete: output %v != %v", out, complete.Output)
+	}
+}
+
+func (m *mockListener) expectStartTotalBytes(t *testing.T, expect int64) {
+	t.Helper()
+
+	if len(m.start) != 1 {
+		t.Fatalf("transfer start was called %d times instead of once", len(m.start))
+	}
+
+	start := m.start[0]
+	if expect != start.TotalBytes {
+		t.Errorf("transfer start: total bytes %v != %v", expect, start.TotalBytes)
+	}
+}
+
+func (m *mockListener) expectCompleteTotalBytes(t *testing.T, expect int64) {
+	t.Helper()
+
+	if len(m.complete) != 1 {
+		t.Fatalf("transfer complete was called %d times instead of once", len(m.complete))
+	}
+
+	complete := m.complete[0]
+	if expect != complete.TotalBytes {
+		t.Errorf("transfer complete: total bytes %v != %v", expect, complete.TotalBytes)
+	}
+}
+
+func (m *mockListener) expectByteTransfers(t *testing.T, expect ...int64) {
+	t.Helper()
+
+	if len(m.start) != 1 {
+		t.Fatalf("transfer start was called %d times instead of once", len(m.start))
+	}
+	if len(m.transfer) != len(expect) {
+		t.Fatalf("transfer start was called %d times instead of expected %d times", len(m.start), len(expect))
+	}
+
+	for i, ex := range expect {
+		if ex != m.transfer[i].BytesTransferred {
+			t.Errorf("transfer call %d: byte count %d != %d", i, ex, m.transfer[i].BytesTransferred)
+		}
 	}
 }
