25b1e4d3598b29b29ff3ad9f2af950781467befa
Luc Talatinian <lucix@amazon.com>
2025-10-14 12:47:53 -0400

    a

--- feature/s3/manager/validate_parts_test.go
+++ feature/s3/manager/validate_parts_test.go
@@ -32,3 +32,21 @@ func TestDownload_RangeMismatch(t *testing.T) {
 		t.Errorf("error mismatch:\n%v !=\n%v", err, "invalid content range")
 	}
 }
+
+func TestDownload_RangeMismatchDisabled(t *testing.T) {
+	c, _, _ := newDownloadBadRangeClient(buf12MB)
+
+	d := manager.NewDownloader(c, func(d *manager.Downloader) {
+		d.Concurrency = 1
+		d.DisableValidateParts = true
+	})
+
+	w := manager.NewWriteAtBuffer(make([]byte, len(buf12MB)))
+	_, err := d.Download(context.Background(), w, &s3.GetObjectInput{
+		Bucket: aws.String("bucket"),
+		Key:    aws.String("key"),
+	})
+	if err != nil {
+		t.Fatalf("expect no err, got %v", err)
+	}
+}
