aad65dcd22b09becda0036e7d2e9c4a6323e0de5
Iliya Lyan <68940374+12ya@users.noreply.github.com>
2026-01-02 01:45:26 +0900

    all: Replace ioutil.TempFile with os.CreateTemp
    Updated all usages of the deprecated ioutil.TempFile to use os.CreateTemp across test and utility files. This change improves compatibility with newer Go versions and follows current best practices for temporary file creation.

--- feature/s3/manager/upload_test.go
+++ feature/s3/manager/upload_test.go
@@ -801,7 +801,7 @@ func TestUploadMaxPartsEOF(t *testing.T) {
 }
 
 func createTempFile(t *testing.T, size int64) (*os.File, func(*testing.T), error) {
-	file, err := ioutil.TempFile(os.TempDir(), aws.SDKName+t.Name())
+	file, err := os.CreateTemp(os.TempDir(), aws.SDKName+t.Name())
 	if err != nil {
 		return nil, nil, err
 	}
