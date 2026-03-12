56f2f26e22271bbd833466b6c1a0fbbecd07ff53
Eren Yeager <92114074+wty-Bryant@users.noreply.github.com>
2026-03-12 16:51:47 -0400

    Add polly SynthesizeSpeech presign missing fields serd (#3344)

--- service/polly/presign_test.go
+++ service/polly/presign_test.go
@@ -14,15 +14,18 @@ import (
 
 func TestPresignOpSynthesizeSpeechInput(t *testing.T) {
 	cases := map[string]struct {
-		LexiconNames []string
-		OutputFormat types.OutputFormat
-		SampleRate   *string
-		Text         *string
-		TextType     types.TextType
-		VoiceID      types.VoiceId
-		ExpectStream string
-		Error        error
-		ExpectError  bool
+		LexiconNames    []string
+		OutputFormat    types.OutputFormat
+		SampleRate      *string
+		Text            *string
+		TextType        types.TextType
+		VoiceID         types.VoiceId
+		Engine          types.Engine
+		LanguageCode    types.LanguageCode
+		SpeechMarkTypes []types.SpeechMarkType
+		ExpectStream    string
+		Error           error
+		ExpectError     bool
 	}{
 		"Single LexiconNames": {
 			LexiconNames: []string{"abc"},
@@ -51,6 +54,41 @@ func TestPresignOpSynthesizeSpeechInput(t *testing.T) {
 			VoiceID:      types.VoiceIdAmy,
 			ExpectStream: "LexiconNames=abc&LexiconNames=mno&OutputFormat=mp3&SampleRate=128&Text=Test+%2FText&TextType=text&VoiceId=Amy",
 		},
+		"With Engine and LanguageCode": {
+			LexiconNames: []string{"abc"},
+			OutputFormat: types.OutputFormatMp3,
+			SampleRate:   aws.String("128"),
+			Text:         aws.String("Test"),
+			TextType:     types.TextTypeText,
+			VoiceID:      types.VoiceIdAmy,
+			Engine:       types.EngineNeural,
+			LanguageCode: types.LanguageCodeArb,
+			ExpectStream: "Engine=neural&LanguageCode=arb&LexiconNames=abc&OutputFormat=mp3&SampleRate=128&Text=Test&TextType=text&VoiceId=Amy",
+		},
+		"With single SpeechMarkType": {
+			OutputFormat:    types.OutputFormatJson,
+			Text:            aws.String("Test"),
+			VoiceID:         types.VoiceIdJoanna,
+			SpeechMarkTypes: []types.SpeechMarkType{types.SpeechMarkTypeSsml},
+			ExpectStream:    "OutputFormat=json&SpeechMarkTypes=ssml&Text=Test&VoiceId=Joanna",
+		},
+		"With multiple SpeechMarkTypes": {
+			OutputFormat:    types.OutputFormatJson,
+			Text:            aws.String("Test"),
+			VoiceID:         types.VoiceIdJoanna,
+			SpeechMarkTypes: []types.SpeechMarkType{types.SpeechMarkTypeSsml, types.SpeechMarkTypeWord, types.SpeechMarkTypeSentence},
+			ExpectStream:    "OutputFormat=json&SpeechMarkTypes=ssml&SpeechMarkTypes=word&SpeechMarkTypes=sentence&Text=Test&VoiceId=Joanna",
+		},
+		"With Engine, LanguageCode and SpeechMarkTypes": {
+			LexiconNames:    []string{"abc", "mno"},
+			OutputFormat:    types.OutputFormatJson,
+			Text:            aws.String("Test"),
+			VoiceID:         types.VoiceIdMatthew,
+			Engine:          types.EngineStandard,
+			LanguageCode:    types.LanguageCodeDeAt,
+			SpeechMarkTypes: []types.SpeechMarkType{types.SpeechMarkTypeWord, types.SpeechMarkTypeViseme},
+			ExpectStream:    "Engine=standard&LanguageCode=de-AT&LexiconNames=abc&LexiconNames=mno&OutputFormat=json&SpeechMarkTypes=word&SpeechMarkTypes=viseme&Text=Test&VoiceId=Matthew",
+		},
 		"Next serializer return error": {
 			Error:       fmt.Errorf("next handler return error"),
 			ExpectError: true,
@@ -62,12 +100,15 @@ func TestPresignOpSynthesizeSpeechInput(t *testing.T) {
 			req := smithyhttp.NewStackRequest().(*smithyhttp.Request)
 			var updatedRequest *smithyhttp.Request
 			param := &SynthesizeSpeechInput{
-				LexiconNames: c.LexiconNames,
-				OutputFormat: c.OutputFormat,
-				SampleRate:   c.SampleRate,
-				Text:         c.Text,
-				TextType:     c.TextType,
-				VoiceId:      c.VoiceID,
+				LexiconNames:    c.LexiconNames,
+				OutputFormat:    c.OutputFormat,
+				SampleRate:      c.SampleRate,
+				Text:            c.Text,
+				TextType:        c.TextType,
+				VoiceId:         c.VoiceID,
+				Engine:          c.Engine,
+				LanguageCode:    c.LanguageCode,
+				SpeechMarkTypes: c.SpeechMarkTypes,
 			}
 
 			m := presignOpSynthesizeSpeechInput{}
