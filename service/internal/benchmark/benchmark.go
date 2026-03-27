463acfd04503d672ac7c393efa6d28044f0fa44a
Luc Talatinian <lucix@amazon.com>
2026-03-27 16:13:49 -0400

    P

--- service/internal/benchmark/benchmark.go
+++ service/internal/benchmark/benchmark.go
@@ -1,3 +1,3 @@
-// package benchmark does nothing. it used to have v1-vs-v2 benchmarks that
+// Package benchmark does nothing. it used to have v1-vs-v2 benchmarks that
 // were unused. it will be removed soon.
 package benchmark
