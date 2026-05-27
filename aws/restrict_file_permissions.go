2f600d12216dc76b0e9e68ba66b251f3c9990b0b
Luc Talatinian <lucix@amazon.com>
2026-05-27 13:38:59 -0400

    lint

--- aws/restrict_file_permissions.go
+++ aws/restrict_file_permissions.go
@@ -5,15 +5,17 @@ package aws
 type RestrictFilePermissions string
 
 const (
-	// Unset.
+	// RestrictFilePermissionsUnset indicates the setting has not been
+	// configured.
 	RestrictFilePermissionsUnset RestrictFilePermissions = ""
 
-	// Sets file permissions to owner read/write only (0600) and directory
-	// permissions to owner only (0700) when creating new cache files and
-	// directories on Unix. This is the default behavior.
-	RestrictFilePermissionsUserReadWrite = "user_read_write"
+	// RestrictFilePermissionsUserReadWrite sets file permissions to owner
+	// read/write only (0600) and directory permissions to owner only (0700)
+	// when creating new cache files and directories on Unix. This is the
+	// default behavior.
+	RestrictFilePermissionsUserReadWrite RestrictFilePermissions = "user_read_write"
 
-	// Does not set any file or directory permissions, relying on the system's
-	// default umask.
-	RestrictFilePermissionsUnrestricted = "unrestricted"
+	// RestrictFilePermissionsUnrestricted does not set any file or directory
+	// permissions, relying on the system's default umask.
+	RestrictFilePermissionsUnrestricted RestrictFilePermissions = "unrestricted"
 )
