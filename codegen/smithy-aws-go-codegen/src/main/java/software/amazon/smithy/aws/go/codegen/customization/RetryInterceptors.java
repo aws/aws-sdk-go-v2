f776a08b589b687906df93457be1b480d97885ae
Tao Tong <taotong@amazon.com>
2026-06-05 21:11:23 +0000

    feat: mark common plugins and fix per-op ordering for invokeOperation refactor

--- codegen/smithy-aws-go-codegen/src/main/java/software/amazon/smithy/aws/go/codegen/customization/RetryInterceptors.java
+++ codegen/smithy-aws-go-codegen/src/main/java/software/amazon/smithy/aws/go/codegen/customization/RetryInterceptors.java
@@ -18,6 +18,7 @@ public class RetryInterceptors implements GoIntegration {
                                 .useClientOptions()
                                 .build()
                 )
+                .isCommon(true)
                 .build();
     }
 
